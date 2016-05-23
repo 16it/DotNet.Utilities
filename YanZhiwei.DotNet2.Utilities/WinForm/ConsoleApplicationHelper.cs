18f7e9a3073db20adf11def97777f43a13c00c58
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-05-23 13:15:43 +0800

    添加‘监听用户关闭控制台应用程序’DetectShutdown方法。

--- YanZhiwei.DotNet2.Utilities/WinForm/ConsoleApplicationHelper.cs
+++ YanZhiwei.DotNet2.Utilities/WinForm/ConsoleApplicationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace YanZhiwei.DotNet2.Utilities.WinForm
 {
@@ -26,6 +27,51 @@ namespace YanZhiwei.DotNet2.Utilities.WinForm
                         inputFactory(_input);
                 }
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        #region 控制台应用程序关闭时的消息捕获
+
+        /// <summary>
+        /// 当用户关闭Console时，系统会发送次消息
+        /// </summary>
+        /// 时间：2016-05-23 11:13
+        /// 备注：
+        private const int CTRL_CLOSE_EVENT = 2;
+
+        /// <summary>
+        /// 控制台控制委托
+        /// </summary>
+        /// <param name="dwCtrlType">Type of the dw control.</param>
+        /// <returns></returns>
+        /// 时间：2016-05-23 11:13
+        /// 备注：
+        public delegate bool ConsoleCtrlHanlder(int dwCtrlType);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleCtrlHandler(ConsoleCtrlHanlder detectShutdown, bool Add);
+
+        #endregion 控制台应用程序关闭时的消息捕获
+
+        /// <summary>
+        /// 监听用户关闭控制台应用程序
+        /// </summary>
+        /// <param name="shutDownConsoleFactory">委托</param>
+        /// 时间：2016-05-23 13:09
+        /// 备注：
+        public static void DetectShutdown(Action shutDownConsoleFactory)
+        {
+            ConsoleCtrlHanlder _shutDownHanlder = new ConsoleCtrlHanlder(ctrlType =>
+            {
+                switch (ctrlType)
+                {
+                    case CTRL_CLOSE_EVENT:
+                        shutDownConsoleFactory();
+                        break;
+                }
+
+                return false;
+            });
+            SetConsoleCtrlHandler(_shutDownHanlder, true);
         }
     }
 }
