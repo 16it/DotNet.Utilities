82b5dfa8f3c522cb38edcd8f75fe1543814638e7
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-06-22 16:52:57 +0800

    增加ToCacheList，ToPageCache方法。

--- YanZhiwei.DotNet.Framework.Data/DbContextBase.cs
+++ YanZhiwei.DotNet.Framework.Data/DbContextBase.cs
@@ -1,10 +1,16 @@
 ﻿namespace YanZhiwei.DotNet.Framework.Data
 {
-    using PetaPoco;
     using System;
+    using System.Collections.Generic;
     using System.Data.Common;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using System.Threading;
+
+    using Core.Cache;
+
+    using PetaPoco;
+
     using YanZhiwei.DotNet.Framework.Contract;
 
     /// <summary>
@@ -14,6 +20,16 @@
     /// 备注：
     public class DbContextBase : Database, IDisposable
     {
+        #region Fields
+
+        public readonly string CacheKeyPrefix = null;
+
+        private static Regex rxParamsPrefix = new Regex(@"(?<!@)@\w+", RegexOptions.Compiled);
+
+        private string _paramPrefix = "@";
+
+        #endregion Fields
+
         #region Constructors
 
         /// <summary>
@@ -24,9 +40,10 @@
         /// <param name="auditLogger">数据添加，删除，修改拦截接口</param>
         /// 时间：2016-03-28 10:13
         /// 备注：
-        public DbContextBase(string connectionString, DbProviderFactory dbProviderFactory, ISqlAuditable auditLogger)
+        public DbContextBase(string connectionString, DbProviderFactory dbProviderFactory, ISqlAuditable auditLogger, string cacheKeyPrefix)
             : base(connectionString, dbProviderFactory)
         {
+            this.CacheKeyPrefix = cacheKeyPrefix;
             this.AuditLogger = auditLogger;
             this.DataChangedEvent += DbContextBase_DataChangedEvent; ;
         }
@@ -38,7 +55,7 @@
         /// 时间：2016-03-28 10:14
         /// 备注：
         public DbContextBase(string connectionString)
-            : this(connectionString, DbProviderFactories.GetFactory("System.Data.SqlClient"), null)
+            : this(connectionString, DbProviderFactories.GetFactory("System.Data.SqlClient"), null, "DotNetFrameworkData")
         {
         }
 
@@ -50,7 +67,7 @@
         /// 时间：2016-03-28 10:15
         /// 备注：
         public DbContextBase(string connectionString, ISqlAuditable auditLogger)
-            : this(connectionString, DbProviderFactories.GetFactory("System.Data.SqlClient"), auditLogger)
+            : this(connectionString, DbProviderFactories.GetFactory("System.Data.SqlClient"), auditLogger, "DotNetFrameworkData")
         {
         }
 
@@ -70,6 +87,52 @@
 
         #region Methods
 
+        /// <summary>
+        /// 将结果转换为缓存的列表，如缓存存在，直接返回，否则从数据源查询，并存入缓存中再返回
+        /// </summary>
+        /// <typeparam name="TSource">泛型</typeparam>
+        /// <param name="sql">sql语句</param>
+        /// <param name="args">sql参数</param>
+        /// <returns>集合</returns>
+        /// 时间：2016/6/22 16:50
+        /// 备注：
+        public List<TSource> ToCacheList<TSource>(string sql, params object[] args)
+        {
+            string _key = GetKey(0, 0, sql, args);
+            List<TSource> _result = (List<TSource>)CacheHelper.Get(_key);
+            if (_result != null)
+            {
+                return _result;
+            }
+            _result = this.Fetch<TSource>(sql, args);
+            CacheHelper.Set(_key, _result);
+            return _result;
+        }
+
+        /// <summary>
+        /// 查询分页数据结果，如缓存存在，直接返回，否则从数据源查找分页结果，并存入缓存中再返回
+        /// </summary>
+        /// <typeparam name="TSource">泛型</typeparam>
+        /// <param name="page">第几页</param>
+        /// <param name="itemsPerPage">每页多少条数据</param>
+        /// <param name="sql">sql语句</param>
+        /// <param name="args">sql参数</param>
+        /// <returns>分页对象</returns>
+        /// 时间：2016/6/22 16:51
+        /// 备注：
+        public Page<TSource> ToPageCache<TSource>(long page, long itemsPerPage, string sql, params object[] args)
+        {
+            string _key = GetKey(page, itemsPerPage, sql, args);
+            Page<TSource> _result = (Page<TSource>)CacheHelper.Get(_key);
+            if (_result != null)
+            {
+                return _result;
+            }
+            _result = this.Page<TSource>(page, itemsPerPage, sql, args);
+            CacheHelper.Set(_key, _result);
+            return _result;
+        }
+
         private void DbContextBase_DataChangedEvent(Type type, string sql)
         {
             if (type != null)
@@ -87,6 +150,26 @@
                 });
                 _task.Start();
             }
+        }
+
+        private string GetKey(long page, long itemsPerPage, string sql, object[] args)
+        {
+            string _key = null;
+            if (args != null)
+            {
+                var _new_args = new List<object>();
+                sql = ProcessParams(sql, args, _new_args);
+                sql = rxParamsPrefix.Replace(sql, m => _paramPrefix + m.Value.Substring(1));
+                sql = sql.Replace("@@", "@");
+                int i = 0;
+                foreach (object arg in args)
+                {
+                    sql = sql.Replace(string.Format("@{0}", i), arg.ToString());
+                    i++;
+                }
+            }
+            _key = string.Format("{0}.{1}{2}{3}", CacheKeyPrefix, page, itemsPerPage, sql);
+            return _key;
         }
 
         #endregion Methods
