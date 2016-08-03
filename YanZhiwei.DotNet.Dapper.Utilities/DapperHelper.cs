4706d4a0c86296e5e911bd8e42982c88448539d1
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-08-03 14:37:51 +0800

    优化代码格式

--- YanZhiwei.DotNet.Dapper.Utilities/DapperHelper.cs
+++ YanZhiwei.DotNet.Dapper.Utilities/DapperHelper.cs
@@ -6,7 +6,7 @@
     using System.Data;
     using System.Data.SqlClient;
     using System.Linq;
-
+    
     /// <summary>
     /// Dapper 数据库操作帮助类，默认是sql Server
     /// </summary>
@@ -15,13 +15,13 @@
     public abstract class DapperHelper
     {
         #region Fields
-
+        
         private string ConnectString = string.Empty;
-
+        
         #endregion Fields
-
+        
         #region Constructors
-
+        
         /// <summary>
         /// 构造函数
         /// </summary>
@@ -33,11 +33,11 @@
             ValidateHelper.Begin().NotNullOrEmpty(connectString, "连接字符串不能为空！");
             ConnectString = connectString;
         }
-
+        
         #endregion Constructors
-
+        
         #region Methods
-
+        
         /// <summary>
         /// 创建SqlConnection连接对象，需要打开
         /// </summary>
@@ -47,11 +47,13 @@
         public virtual IDbConnection CreateConnection()
         {
             IDbConnection sqlConnection = new SqlConnection(ConnectString);
-            if (sqlConnection.State != ConnectionState.Open)
+            
+            if(sqlConnection.State != ConnectionState.Open)
                 sqlConnection.Open();
+                
             return sqlConnection;
         }
-
+        
         /// <summary>
         /// 返回DataTable
         /// </summary>
@@ -63,18 +65,19 @@
         /// 时间：2016-01-19 16:22
         /// 备注：
         public virtual DataTable ExecuteDataTable<T>(IDbConnection dbConnection, string sql, T parameters)
-            where T : class
+        where T : class
         {
-            if (dbConnection == null)
+            if(dbConnection == null)
                 dbConnection = CreateConnection();
-            using (IDbConnection sqlConnection = dbConnection)
+                
+            using(IDbConnection sqlConnection = dbConnection)
             {
                 DataTable _table = new DataTable();
                 _table.Load(sqlConnection.ExecuteReader(sql, parameters));
                 return _table;
             }
         }
-
+        
         /// <summary>
         /// 返回DataTable
         /// </summary>
@@ -85,11 +88,11 @@
         /// 时间：2016-01-19 16:23
         /// 备注：
         public virtual DataTable ExecuteDataTable<T>(string sql, T parameters)
-            where T : class
+        where T : class
         {
             return ExecuteDataTable<T>(null, sql, parameters);
         }
-
+        
         /// <summary>
         /// 执行操作
         /// </summary>
@@ -101,16 +104,17 @@
         /// 时间：2016-01-19 16:23
         /// 备注：
         public virtual int ExecuteNonQuery<T>(IDbConnection dbConnection, string sql, T parameters)
-            where T : class
+        where T : class
         {
-            if (dbConnection == null)
+            if(dbConnection == null)
                 dbConnection = CreateConnection();
-            using (IDbConnection sqlConnection = dbConnection)
+                
+            using(IDbConnection sqlConnection = dbConnection)
             {
                 return sqlConnection.Execute(sql, parameters);
             }
         }
-
+        
         /// <summary>
         /// 执行操作
         /// </summary>
@@ -121,11 +125,11 @@
         /// 时间：2016-01-19 16:23
         /// 备注：
         public virtual int ExecuteNonQuery<T>(string sql, T parameters)
-            where T : class
+        where T : class
         {
             return ExecuteNonQuery<T>(null, sql, parameters);
         }
-
+        
         /// <summary>
         /// 返回IDataReader
         /// </summary>
@@ -137,15 +141,16 @@
         /// 时间：2016-01-19 16:24
         /// 备注：
         public virtual IDataReader ExecuteReader<T>(IDbConnection dbConnection, string sql, T parameters)
-            where T : class
+        where T : class
         {
-            if (dbConnection == null)
+            if(dbConnection == null)
                 dbConnection = CreateConnection();
+                
             IDbConnection sqlConnection = dbConnection;
-
+            
             return sqlConnection.ExecuteReader(sql, parameters);
         }
-
+        
         /// <summary>
         /// 返回IDataReader
         /// </summary>
@@ -156,11 +161,11 @@
         /// 时间：2016-01-19 16:24
         /// 备注：
         public virtual IDataReader ExecuteReader<T>(string sql, T parameters)
-            where T : class
+        where T : class
         {
             return ExecuteReader<T>(null, sql, parameters);
         }
-
+        
         /// <summary>
         /// 返回对象
         /// </summary>
@@ -172,16 +177,17 @@
         /// 时间：2016-01-19 16:25
         /// 备注：
         public virtual object ExecuteScalar<T>(IDbConnection dbConnection, string sql, T parameters)
-            where T : class
+        where T : class
         {
-            if (dbConnection == null)
+            if(dbConnection == null)
                 dbConnection = CreateConnection();
-            using (IDbConnection sqlConnection = dbConnection)
+                
+            using(IDbConnection sqlConnection = dbConnection)
             {
                 return sqlConnection.ExecuteScalar(sql, parameters, null, null, null);
             }
         }
-
+        
         /// <summary>
         /// 返回对象
         /// </summary>
@@ -192,11 +198,11 @@
         /// 时间：2016-01-19 16:25
         /// 备注：
         public virtual object ExecuteScalar<T>(string sql, T parameters)
-            where T : class
+        where T : class
         {
             return ExecuteScalar<T>(null, sql, parameters);
         }
-
+        
         /// <summary>
         /// 返回实体类
         /// </summary>
@@ -208,19 +214,20 @@
         /// 时间：2016-01-19 16:25
         /// 备注：
         public virtual T Query<T>(IDbConnection dbConnection, string sql, T parameters)
-            where T : class
+        where T : class
         {
-            if (dbConnection == null)
+            if(dbConnection == null)
                 dbConnection = CreateConnection();
+                
             T _result = null;
-            using (IDbConnection sqlConnection = dbConnection)
+            using(IDbConnection sqlConnection = dbConnection)
             {
                 _result = sqlConnection.Query<T>(sql, parameters).FirstOrDefault();
             }
-
+            
             return _result;
         }
-
+        
         /// <summary>
         /// 返回实体类
         /// </summary>
@@ -231,11 +238,11 @@
         /// 时间：2016-01-19 16:25
         /// 备注：
         public virtual T Query<T>(string sql, T parameters)
-            where T : class
+        where T : class
         {
             return Query<T>(null, sql, parameters);
         }
-
+        
         /// <summary>
         /// 返回集合
         /// </summary>
@@ -247,19 +254,20 @@
         /// 时间：2016-01-19 16:25
         /// 备注：
         public virtual List<T> QueryList<T>(IDbConnection dbConnection, string sql, T parameters)
-            where T : class
+        where T : class
         {
-            if (dbConnection == null)
+            if(dbConnection == null)
                 dbConnection = CreateConnection();
+                
             List<T> _result = null;
-            using (IDbConnection sqlConnection = dbConnection)
+            using(IDbConnection sqlConnection = dbConnection)
             {
                 _result = sqlConnection.Query<T>(sql, parameters).ToList();
             }
-
+            
             return _result;
         }
-
+        
         /// <summary>
         /// 返回集合，默认Sql Server数据库
         /// </summary>
@@ -270,11 +278,11 @@
         /// 时间：2016-01-19 16:25
         /// 备注：
         public virtual List<T> QueryList<T>(string sql, T parameters)
-            where T : class
+        where T : class
         {
             return QueryList<T>(null, sql, parameters);
         }
-
+        
         #endregion Methods
     }
 }
