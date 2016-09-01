561b5bcd076787373bef3b6d177115832da754f0
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-09-01 14:39:49 +0800

    coding

--- YanZhiwei.DotNet3.5.UtilitiesExamples/Program.cs
+++ YanZhiwei.DotNet3.5.UtilitiesExamples/Program.cs
@@ -4,20 +4,20 @@ namespace YanZhiwei.DotNet3._5.Utilities.Examples
 {
     internal class Program
     {
-        //private static void Main(string[] args)
-        //{
-        //    try
-        //    {
-        //        TaskExamle.Demo1();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine("Error:" + ex.Message);
-        //    }
-        //    finally
-        //    {
-        //        Console.ReadLine();
-        //    }
-        //}
+        private static void Main(string[] args)
+        {
+            try
+            {
+                TaskExamle.Demo1();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
     }
 }
