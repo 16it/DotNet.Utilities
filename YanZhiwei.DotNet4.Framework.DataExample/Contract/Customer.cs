d0ccb86996532632ddd5804c84262d5dd891c892
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-09-06 10:09:07 +0800

    coding

--- YanZhiwei.DotNet4.Framework.DataExample/Contract/Customer.cs
+++ YanZhiwei.DotNet4.Framework.DataExample/Contract/Customer.cs
@@ -12,26 +12,70 @@ namespace YanZhiwei.DotNet4.Framework.Data.Example.Contract
     {
         [Required]
         [StringLength(5)]
-        public string CustomerID { get; set; }
+        public string CustomerID
+        {
+            get;
+            set;
+        }
 
         [Required]
         [StringLength(40)]
-        public string CompanyName { get; set; }
+        public string CompanyName
+        {
+            get;
+            set;
+        }
 
         [Required]
         [StringLength(30)]
-        public string ContactName { get; set; }
+        public string ContactName
+        {
+            get;
+            set;
+        }
 
         [Required]
         [StringLength(30)]
-        public string ContactTitle { get; set; }
+        public string ContactTitle
+        {
+            get;
+            set;
+        }
 
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
-        public string Phone { get; set; }
-        public string Fax { get; set; }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+            get;
+            set;
+        }
+        public string Region
+        {
+            get;
+            set;
+        }
+        public string PostalCode
+        {
+            get;
+            set;
+        }
+        public string Country
+        {
+            get;
+            set;
+        }
+        public string Phone
+        {
+            get;
+            set;
+        }
+        public string Fax
+        {
+            get;
+            set;
+        }
     }
 }
--- YanZhiwei.DotNet4.Framework.DataExample/Contract/CustomerRequest.cs
+++ YanZhiwei.DotNet4.Framework.DataExample/Contract/CustomerRequest.cs
@@ -4,7 +4,15 @@ namespace YanZhiwei.DotNet4.Framework.Data.Example.Contract
 {
     public class CustomerRequest : BusinessRequest
     {
-        public string CustomerID { get; set; }
-        public string ContactName { get; set; }
+        public string CustomerID
+        {
+            get;
+            set;
+        }
+        public string ContactName
+        {
+            get;
+            set;
+        }
     }
 }
