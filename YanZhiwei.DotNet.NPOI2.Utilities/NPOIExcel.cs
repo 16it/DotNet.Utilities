ebfcfc602cb8810c00b408f0f35c7ca06e3d074d
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-10-11 17:26:17 +0800

    coding

--- YanZhiwei.DotNet.NPOI2.Utilities/NPOIExcel.cs
+++ YanZhiwei.DotNet.NPOI2.Utilities/NPOIExcel.cs
@@ -3,11 +3,12 @@
     using System.Data;
     using System.IO;
     
+    using DotNet2.Utilities.Operator;
+    
     using NPOI.HSSF.UserModel;
     using NPOI.SS.UserModel;
     using NPOI.SS.Util;
-    
-    using DotNet2.Utilities.Operator;
+    using NPOI.XSSF.UserModel;
     
     /// <summary>
     /// NPOIExcel 操作辅助类
@@ -17,6 +18,103 @@
     public class NPOIExcel
     {
         #region Methods
+        
+        /// <summary>
+        /// 将EXCEL文件导入到DataTable
+        /// </summary>
+        /// <param name="filePath">EXCEL路径</param>
+        /// <param name="sheetIndex">Sheet索引</param>
+        /// <param name="headIndex">列索引</param>
+        /// <param name="rowIndex">行起始索引</param>
+        /// <returns></returns>
+        /// 时间：2016/10/11 17:07
+        /// 备注：
+        public static DataTable ToDataTable(string filePath, ushort sheetIndex, ushort headIndex, ushort rowIndex)
+        {
+            ValidateOperator.Begin().NotNull(filePath, "需要导入到EXCEL文件路径").IsFilePath(filePath).CheckFileExists(filePath);
+            DataTable _table = new DataTable();
+            IWorkbook _hssfworkbook;
+            using(FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                _hssfworkbook = WorkbookFactory.Create(file);
+            }
+            ISheet _sheet = _hssfworkbook.GetSheetAt(sheetIndex);
+            AddDataColumns(_sheet, headIndex, _table);
+            bool _supportFormula = string.Compare(Path.GetExtension(filePath), ".xlsx", true) == 0;
+            
+            for(int i = (_sheet.FirstRowNum + rowIndex); i <= _sheet.LastRowNum; i++)
+            {
+                IRow _row = _sheet.GetRow(i);
+                bool _emptyRow = true;
+                
+                if(_row == null) continue;
+                
+                object[] _itemArray = new object[_row.LastCellNum];
+                
+                for(int j = _row.FirstCellNum; j < _row.LastCellNum; j++)
+                {
+                    if(_row.GetCell(j) == null) continue;
+                    
+                    switch(_row.GetCell(j).CellType)
+                    {
+                        case CellType.Numeric:
+                            if(DateUtil.IsCellDateFormatted(_row.GetCell(j)))       //日期类型
+                            {
+                                _itemArray[j] = _row.GetCell(j).DateCellValue;
+                            }
+                            else//其他数字类型
+                            {
+                                _itemArray[j] = _row.GetCell(j).NumericCellValue;
+                            }
+                            
+                            break;
+                            
+                        case CellType.Blank:
+                            _itemArray[j] = string.Empty;
+                            break;
+                            
+                        case CellType.Formula:
+                            IFormulaEvaluator _eva = null;
+                            
+                            if(_supportFormula)
+                            {
+                                _eva = new XSSFFormulaEvaluator(_hssfworkbook);
+                            }
+                            else
+                            {
+                                _eva = new HSSFFormulaEvaluator(_hssfworkbook);
+                            }
+                            
+                            if(_eva.Evaluate(_row.GetCell(j)).CellType == CellType.Numeric)
+                            {
+                                _itemArray[j] = _eva.Evaluate(_row.GetCell(j)).NumberValue;
+                            }
+                            else
+                            {
+                                _itemArray[j] = _eva.Evaluate(_row.GetCell(j)).StringValue;
+                            }
+                            
+                            break;
+                            
+                        default:
+                            _itemArray[j] = _row.GetCell(j).StringCellValue;
+                            break;
+                    }
+                    
+                    if(_itemArray[j] != null && !string.IsNullOrEmpty(_itemArray[j].ToString().Trim()))
+                    {
+                        _emptyRow = false;
+                    }
+                }
+                
+                if(!_emptyRow)
+                {
+                    _table.Rows.Add(_itemArray);
+                }
+            }
+            
+            return _table;
+        }
         
         /// <summary>
         /// 导出到Excel
@@ -73,6 +171,21 @@
                 //写入数据流
                 _workBook.Write(fileStream);
                 return true;
+            }
+        }
+        
+        private static void AddDataColumns(ISheet sheet, ushort headIndex, DataTable table)
+        {
+            IRow _headRow = sheet.GetRow(headIndex);
+            
+            if(_headRow != null)
+            {
+                int _colCount = _headRow.LastCellNum;
+                
+                for(int i = 0; i < _colCount; i++)
+                {
+                    table.Columns.Add(_headRow.GetCell(i).StringCellValue);
+                }
             }
         }
         
--- YanZhiwei.DotNet.NPOI2.UtilitiesTests/NPOIExcelTests.cs
+++ YanZhiwei.DotNet.NPOI2.UtilitiesTests/NPOIExcelTests.cs
@@ -2,8 +2,9 @@
 using System;
 using System.Data;
 using System.IO;
-using YanZhiwei.DotNet2.Utilities.DataOperator;
 using YanZhiwei.DotNet2.Utilities.Common;
+using YanZhiwei.DotNet2.Utilities.DataOperator;
+
 namespace YanZhiwei.DotNet.NPOI2.Utilities.Tests
 {
     [TestClass()]
@@ -25,6 +26,14 @@ namespace YanZhiwei.DotNet.NPOI2.Utilities.Tests
             NPOIExcel.ToExcel(_queryResult, "Hello", "Employees", _xlsPath);
             bool _actual = File.Exists(_xlsPath);
             Assert.IsTrue(_actual);
+        }
+        
+        [TestMethod()]
+        public void ToDataTableTest()
+        {
+            string _path = @"D:\Employees_20161011165659.xls";
+            DataTable _table = NPOIExcel.ToDataTable(_path, 0, 1, 2);
+            Assert.IsTrue(_table.Rows.Count > 0);
         }
     }
 }
