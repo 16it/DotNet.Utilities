c49eddfd0e49a5a4a0ccc853920e1575e3b9376b
YanZh <YanZh@YANZHIWEI-PC>
2016-10-15 22:54:45 +0800

    coding

---
+++ YanZhiwei.DotNet3.5.Utilities.Tests/Mapper/GeneralMapperTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data;
+using YanZhiwei.DotNet2.Utilities.DataOperator;
+using System.Linq;
+namespace YanZhiwei.DotNet3._5.Utilities.Mapper.Tests
+{
+    [TestClass()]
+    public class GeneralMapperTests
+    {
+        [TestMethod()]
+        public void ToListTest()
+        {
+            SqlServerDataOperator _sqlHelper = new SqlServerDataOperator(@"server=YANZHIWEI-PC\SQLEXPRESS;database=AdventureWorks2014;uid=sa;pwd=sasa;");
+            string _sql = @"SELECT [AddressID]
+                                  ,[AddressLine1]
+                                  ,[AddressLine2]
+                                  ,[City]
+                                  ,[StateProvinceID]
+                                  ,[PostalCode]
+                                  ,[rowguid]
+                                  ,[ModifiedDate]
+                                  FROM [Person].[Address]";
+            DataTable _result = _sqlHelper.ExecuteDataTable(_sql, null);
+            IEnumerable<Address> _actual = GeneralMapper.ToList<Address>(_result);
+            Assert.IsTrue(_actual.Count() > 0);
+        }
+    }
+}
---
+++ YanZhiwei.DotNet3.5.Utilities/Mapper/GeneralMapper.cs
@@ -0,0 +1,83 @@
+﻿namespace YanZhiwei.DotNet3._5.Utilities.Mapper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Reflection;
+    
+    /// <summary>
+    /// 通用类型映射转换
+    /// </summary>
+    /// 时间:2016/10/15 22:34
+    /// 备注:
+    public class GeneralMapper
+    {
+        #region Fields
+        
+        private static readonly IDictionary<Type, ICollection<PropertyInfo>> PropertiesCache =
+            new Dictionary<Type, ICollection<PropertyInfo>>();
+            
+        #endregion Fields
+        
+        #region Methods
+        
+        /// <summary>
+        /// 将DataTable导出成集合
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="table">需要导出的DataTable</param>
+        /// <returns>集合</returns>
+        /// 时间:2016/10/15 22:41
+        /// 备注:
+        public static IEnumerable<T> ToList<T>(DataTable table)
+        where T : class, new()
+        {
+            try
+            {
+                Type _objType = typeof(T);
+                ICollection<PropertyInfo> _properties;
+                
+                lock(PropertiesCache)
+                {
+                    if(!PropertiesCache.TryGetValue(_objType, out _properties))
+                    {
+                        _properties = _objType.GetProperties().Where(property => property.CanWrite).ToList();
+                        PropertiesCache.Add(_objType, _properties);
+                    }
+                }
+                
+                List<T> _list = new List<T>(table.Rows.Count);
+                IEnumerable<DataRow> _dataRowList = table.AsEnumerable();
+                
+                foreach(DataRow row in _dataRowList)
+                {
+                    T _objItem = new T();
+                    
+                    foreach(PropertyInfo prop in _properties)
+                    {
+                        try
+                        {
+                            Type _propType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+                            object _safeValue = row[prop.Name] == null ? null : Convert.ChangeType(row[prop.Name], _propType);
+                            prop.SetValue(_objItem, _safeValue, null);
+                        }
+                        catch
+                        {
+                        }
+                    }
+                    
+                    _list.Add(_objItem);
+                }
+                
+                return _list;
+            }
+            catch
+            {
+                return Enumerable.Empty<T>();
+            }
+        }
+        
+        #endregion Methods
+    }
+}
