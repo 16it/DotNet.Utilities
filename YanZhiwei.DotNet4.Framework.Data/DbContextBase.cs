bc787ea3b9a0cb0bd0ace954c3657ad8132ca46f
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-11-07 15:16:12 +0800

    coding

--- YanZhiwei.DotNet4.Framework.Data/DbContextBase.cs
+++ YanZhiwei.DotNet4.Framework.Data/DbContextBase.cs
@@ -14,7 +14,7 @@
     using System.Linq;
     using System.Linq.Expressions;
     using System.Threading.Tasks;
-
+    
     /// <summary>
     /// DAL基类，实现Repository通用泛型数据访问模式
     /// </summary>
@@ -30,7 +30,7 @@
             Configuration.LazyLoadingEnabled = false;
             Configuration.ProxyCreationEnabled = false;
         }
-
+        
         /// <summary>
         /// 构造函数
         /// </summary>
@@ -41,7 +41,7 @@
         {
             AuditLogger = auditLogger;
         }
-
+        
         /// <summary>
         /// 日志接口
         /// </summary>
@@ -50,86 +50,91 @@
             get;
             set;
         }
-
+        
         /// <summary>
         /// 删除
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="entity">实体类</param>
-        public void Delete<T>(T entity)
-        where T : ModelBase
+        public void Delete<T, F>(T entity)
+        where T : ModelBase<F>
         {
             this.Entry<T>(entity).State = EntityState.Deleted;
             this.SaveChanges();
         }
-
-
-
-
+        
+        
+        
+        
         /// <summary>
         /// 查找
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="keyValues">查询依据的键</param>
         /// <returns>
         /// 实体类
         /// </returns>
-        public T Find<T>(params object[] keyValues)
-        where T : ModelBase
+        public T Find<T, F>(params object[] keyValues)
+        where T : ModelBase<F>
         {
             return this.Set<T>().Find(keyValues);
         }
-
+        
         /// <summary>
         /// 查找全部
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="conditions">委托.</param>
         /// <returns>
         /// 集合
         /// </returns>
-        public List<T> FindAll<T>(Expression<Func<T, bool>> conditions = null)
-        where T : ModelBase
+        public List<T> FindAll<T, F>(Expression<Func<T, bool>> conditions = null)
+        where T : ModelBase<F>
         {
             if(conditions == null)
                 return this.Set<T>().ToList();
             else
                 return this.Set<T>().Where(conditions).ToList();
         }
-
+        
         /// <summary>
         /// 分页查找
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
         /// <typeparam name="S">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="conditions">查找条件</param>
         /// <param name="orderBy">排序</param>
         /// <param name="pageSize">分页大小</param>
         /// <param name="pageIndex">分页集合</param>
         /// <returns>PagedList</returns>
-        public PagedList<T> FindAllByPage<T, S>(Expression<Func<T, bool>> conditions, Expression<Func<T, S>> orderBy, int pageSize, int pageIndex)
-        where T : ModelBase
+        public PagedList<T> FindAllByPage<T, S, F>(Expression<Func<T, bool>> conditions, Expression<Func<T, S>> orderBy, int pageSize, int pageIndex)
+        where T : ModelBase<F>
         {
             var queryList = conditions == null ? this.Set<T>() : this.Set<T>().Where(conditions) as IQueryable<T>;
             return queryList.OrderByDescending(orderBy).ToPagedList(pageIndex, pageSize);
         }
-
+        
         /// <summary>
         /// 添加
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="entity">实体类</param>
         /// <returns>
         /// 实体类
         /// </returns>
-        public T Insert<T>(T entity)
-        where T : ModelBase
+        public T Insert<T, F>(T entity)
+        where T : ModelBase<F>
         {
             this.Set<T>().Add(entity);
             this.SaveChanges();
             return entity;
         }
-
+        
         /// <summary>
         /// 将在此上下文中所做的所有更改保存到基础数据库。
         /// </summary>
@@ -142,7 +147,7 @@
             var result = base.SaveChanges();
             return result;
         }
-
+        
         /// <summary>
         /// Sql语句查询
         /// </summary>
@@ -154,17 +159,18 @@
         {
             return this.Database.SqlQuery<T>(sql, parameters);
         }
-
+        
         /// <summary>
         /// 更新
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="entity">实体类</param>
         /// <returns>
         /// 实体类
         /// </returns>
-        public T Update<T>(T entity)
-        where T : ModelBase
+        public T Update<T, F>(T entity)
+        where T : ModelBase<F>
         {
             var set = this.Set<T>();
             set.Attach(entity);
@@ -172,7 +178,7 @@
             this.SaveChanges();
             return entity;
         }
-
+        
         /// <summary>
         /// 日志拦截写入
         /// </summary>
@@ -180,14 +186,14 @@
         {
             if(this.AuditLogger == null)
                 return;
-
+                
             foreach(var dbEntry in this.ChangeTracker.Entries<ModelBase>().Where(p => p.State == EntityState.Added || p.State == EntityState.Deleted || p.State == EntityState.Modified))
             {
                 AuditableAttribute _auditableAttr = dbEntry.Entity.GetType().GetCustomAttributes(typeof(AuditableAttribute), false).SingleOrDefault() as AuditableAttribute;
-
+                
                 if(_auditableAttr == null)
                     continue;
-
+                    
                 string _operaterName = ServiceCallContext.Current.Operater.Name;
                 Task.Factory.StartNew(() =>
                 {
@@ -198,7 +204,7 @@
                 });
             }
         }
-
-
+        
+        
     }
 }
--- YanZhiwei.DotNet4.Framework.Data/IDataRepository.cs
+++ YanZhiwei.DotNet4.Framework.Data/IDataRepository.cs
@@ -5,7 +5,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq.Expressions;
-
+    
     /// <summary>
     /// 仓储接口
     /// </summary>
@@ -14,44 +14,48 @@
     public interface IDataRepository
     {
         #region Methods
-
+        
         /// <summary>
         /// 删除
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="entity">实体类</param>
         /// 时间：2016-01-13 13:31
         /// 备注：
-        void Delete<T>(T entity)
-        where T : ModelBase;
-
+        void Delete<T, F>(T entity)
+        where T : ModelBase<F>;
+        
         /// <summary>
         /// 查找
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="keyValues">删除依据的键</param>
         /// <returns>实体类</returns>
         /// 时间：2016-01-13 13:32
         /// 备注：
-        T Find<T>(params object[] keyValues)
-        where T : ModelBase;
-
+        T Find<T, F>(params object[] keyValues)
+        where T : ModelBase<F>;
+        
         /// <summary>
         /// 查找全部
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="conditions">委托.</param>
         /// <returns>集合</returns>
         /// 时间：2016-01-13 13:32
         /// 备注：
-        List<T> FindAll<T>(Expression<Func<T, bool>> conditions = null)
-        where T : ModelBase;
-
+        List<T> FindAll<T, F>(Expression<Func<T, bool>> conditions = null)
+        where T : ModelBase<F>;
+        
         /// <summary>
         /// 分页查找
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
         /// <typeparam name="S">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="conditions">查找条件</param>
         /// <param name="orderBy">排序</param>
         /// <param name="pageSize">分页大小</param>
@@ -59,31 +63,33 @@
         /// <returns>PagedList</returns>
         /// 时间：2016-01-13 13:33
         /// 备注：
-        PagedList<T> FindAllByPage<T, S>(Expression<Func<T, bool>> conditions, Expression<Func<T, S>> orderBy, int pageSize, int pageIndex)
-        where T : ModelBase;
-
+        PagedList<T> FindAllByPage<T, S, F>(Expression<Func<T, bool>> conditions, Expression<Func<T, S>> orderBy, int pageSize, int pageIndex)
+        where T : ModelBase<F>;
+        
         /// <summary>
         /// 添加
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="entity">实体类</param>
         /// <returns>实体类</returns>
         /// 时间：2016-01-13 13:31
         /// 备注：
-        T Insert<T>(T entity)
-        where T : ModelBase;
-
+        T Insert<T, F>(T entity)
+        where T : ModelBase<F>;
+        
         /// <summary>
         /// 更新
         /// </summary>
         /// <typeparam name="T">泛型</typeparam>
+        /// <typeparam name="F">主键泛型</typeparam>
         /// <param name="entity">实体类</param>
         /// <returns>实体类</returns>
         /// 时间：2016-01-13 13:31
         /// 备注：
-        T Update<T>(T entity)
-        where T : ModelBase;
-
+        T Update<T, F>(T entity)
+        where T : ModelBase<F>;
+        
         #endregion Methods
     }
 }
