4c8f5ac829d9cbde7954a510ab7bd85c0089c667
YanZhiwei <Yan.Zhiwei@hotmail.com>
2016-11-23 16:01:21 +0800

    coding

---
+++ YanZhiwei.DotNet2.Utilities.Tests/Builder/CRCBuilderTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YanZhiwei.DotNet2.Utilities.Common;
+
+namespace YanZhiwei.DotNet2.Utilities.Builder.Tests
+{
+    [TestClass()]
+    public class CRCBuilderTests
+    {
+        [TestMethod()]
+        public void Create16MODBUSTest()
+        {
+            byte[] _testData = new byte[16] { 0x05, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x86, 0x80, 0x00, 0x02, 0xFF, 0xFF };
+            byte[] _expect = new byte[2] { 0x3C, 0xA4 };
+            byte[] _actual = ByteHelper.ToBytes(CRCBuilder.Create16MODBUS(_testData), false);
+            CollectionAssert.AreEqual(_expect, _actual);
+        }
+    }
+}
---
+++ YanZhiwei.DotNet2.Utilities/Builder/CRCBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace YanZhiwei.DotNet2.Utilities.Builder
+{
+    /// <summary>
+    /// CRC 辅助类
+    /// </summary>
+    /// 时间：2016/11/23 15:43
+    /// 备注：
+    public static class CRCBuilder
+    {
+        /// <summary>
+        /// CRC-16 MODBUS实现
+        /// </summary>
+        /// <param name="data">需要计算得数组</param>
+        /// <returns>CRC数值</returns>
+        /// 时间：2016/11/23 15:43
+        /// 备注：
+        public static ushort Create16MODBUS(byte[] data)
+        {
+            ushort _ax = 0xFFFF;
+            ushort _lsb = 0;
+            
+            for(int i = 0; i < data.Length; i++)
+            {
+                _ax ^= data[i];
+                
+                for(int j = 0; j < 8; j++)
+                {
+                    _lsb = Convert.ToUInt16(_ax & 0x0001);
+                    _ax = Convert.ToUInt16(_ax >> 1);
+                    
+                    if(_lsb != 0)
+                        _ax ^= 0xA001;
+                }
+            }
+            
+            return _ax;
+        }
+    }
+}
