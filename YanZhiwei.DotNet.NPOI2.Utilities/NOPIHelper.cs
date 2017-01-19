597bc4bf5f8f3ff752ad64898c65b398d2bf371c
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-01-19 16:42:21 +0800

    coding

--- YanZhiwei.DotNet.NPOI2.Utilities/NOPIHelper.cs
+++ YanZhiwei.DotNet.NPOI2.Utilities/NOPIHelper.cs
@@ -1,10 +1,12 @@
-﻿using NPOI.SS.UserModel;
-using System.IO;
-using YanZhiwei.DotNet2.Utilities.Common;
-using YanZhiwei.DotNet2.Utilities.Operator;
-
-namespace YanZhiwei.DotNet.NPOI2.Utilities
+﻿namespace YanZhiwei.DotNet.NPOI2.Utilities
 {
+    using System.IO;
+
+    using DotNet2.Utilities.Common;
+    using DotNet2.Utilities.Operator;
+
+    using NPOI.SS.UserModel;
+
     /// <summary>
     /// NOPI 操作辅助类
     /// </summary>
@@ -12,6 +14,8 @@ namespace YanZhiwei.DotNet.NPOI2.Utilities
     /// 备注：
     public static class NOPIHelper
     {
+        #region Methods
+
         /// <summary>
         /// 获取Excel IWorkbook对象
         /// </summary>
@@ -21,15 +25,29 @@ namespace YanZhiwei.DotNet.NPOI2.Utilities
         {
             ValidateOperator.Begin().NotNull(filePath, "需要导入到EXCEL文件路径").IsFilePath(filePath).CheckFileExists(filePath).CheckedFileExt(Path.GetExtension(filePath), FileExtInfoHelper.ExcelExt);
             IWorkbook _hssfworkbook;
-            
+
             using(FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
                 _hssfworkbook = WorkbookFactory.Create(file);
             }
-            
+
             return _hssfworkbook;
         }
-        
+
+        /// <summary>
+        /// 设置单元格背景色
+        /// </summary>
+        /// <param name="cellStyle">ICellStyle</param>
+        /// <param name="colorRBGValue">颜色RBG数值</param>
+        public static void SetCellBackColor(this ICellStyle cellStyle, short colorRBGValue)
+        {
+            if(cellStyle != null)
+            {
+                cellStyle.FillForegroundColor = colorRBGValue;
+                cellStyle.FillPattern = FillPattern.SolidForeground;
+            }
+        }
+
         /// <summary>
         /// 设置单元格包含边框
         /// </summary>
@@ -44,21 +62,7 @@ namespace YanZhiwei.DotNet.NPOI2.Utilities
                 cellStyle.BorderTop = BorderStyle.Thin;
             }
         }
-        
-        /// <summary>
-        /// 设置单元格背景色
-        /// </summary>
-        /// <param name="cellStyle">ICellStyle</param>
-        /// <param name="colorRBGValue">颜色RBG数值</param>
-        public static void SetCellBackColor(this ICellStyle cellStyle, short colorRBGValue)
-        {
-            if(cellStyle != null)
-            {
-                cellStyle.FillForegroundColor = colorRBGValue;
-                cellStyle.FillPattern = FillPattern.SolidForeground;
-            }
-        }
-        
+
         /// <summary>
         /// 设置单元格文本居中
         /// </summary>
@@ -71,7 +75,37 @@ namespace YanZhiwei.DotNet.NPOI2.Utilities
                 cellStyle.Alignment = HorizontalAlignment.Center;
             }
         }
-        
+
+        /// <summary>
+        /// 当value大于零的时候才插入值
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <param name="value"></param>
+        public static void SetCellValueOnlyThanZero(this ICell cell, int value)
+        {
+            if(cell == null) return;
+
+            if(value > 0)
+            {
+                cell.SetCellValue(value);
+            }
+        }
+
+        /// <summary>
+        /// 当value大于零的时候才插入值
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <param name="value"></param>
+        public static void SetCellValueOnlyThanZero(this ICell cell, double value)
+        {
+            if(cell == null) return;
+
+            if(value > 0)
+            {
+                cell.SetCellValue(value);
+            }
+        }
+
         /// <summary>
         /// 样式创建
         /// eg:
@@ -98,41 +132,13 @@ namespace YanZhiwei.DotNet.NPOI2.Utilities
                 for(int u = row.FirstCellNum; u < row.LastCellNum; u++)
                 {
                     ICell _cell = row.GetCell(u);
-                    
+
                     if(_cell != null)
                         _cell.CellStyle = cellStyle;
                 }
             }
         }
-        
-        /// <summary>
-        /// 当value大于零的时候才插入值
-        /// </summary>
-        /// <param name="cell"></param>
-        /// <param name="value"></param>
-        public static void SetCellValueOnlyThanZero(this ICell cell, int value)
-        {
-            if(cell == null) return;
-            
-            if(value > 0)
-            {
-                cell.SetCellValue(value);
-            }
-        }
-        
-        /// <summary>
-        /// 当value大于零的时候才插入值
-        /// </summary>
-        /// <param name="cell"></param>
-        /// <param name="value"></param>
-        public static void SetCellValueOnlyThanZero(this ICell cell, double value)
-        {
-            if(cell == null) return;
-            
-            if(value > 0)
-            {
-                cell.SetCellValue(value);
-            }
-        }
+
+        #endregion Methods
     }
 }
