d8cb6d7c4198e1952bc75c22d93248977ce681dc
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-01-19 16:44:53 +0800

    coding

--- YanZhiwei.DotNet.NPOI2.Utilities/NPOIExcel.cs
+++ YanZhiwei.DotNet.NPOI2.Utilities/NPOIExcel.cs
@@ -1,13 +1,15 @@
 ﻿namespace YanZhiwei.DotNet.NPOI2.Utilities
 {
+    using System.Data;
+    using System.IO;
+
     using DotNet2.Utilities.Operator;
+
     using NPOI.HSSF.UserModel;
     using NPOI.SS.UserModel;
     using NPOI.SS.Util;
     using NPOI.XSSF.UserModel;
-    using System.Data;
-    using System.IO;
-    
+
     /// <summary>
     /// NPOIExcel 操作辅助类
     /// </summary>
@@ -16,7 +18,7 @@
     public class NPOIExcel
     {
         #region Methods
-        
+
         /// <summary>
         /// 将EXCEL文件导入到DataTable
         /// </summary>
@@ -34,20 +36,20 @@
             ISheet _sheet = _hssfworkbook.GetSheetAt(sheetIndex);
             AddDataColumns(_sheet, headIndex, _table);
             bool _supportFormula = string.Compare(Path.GetExtension(filePath), ".xlsx", true) == 0;
-            
+
             for(int i = (_sheet.FirstRowNum + rowIndex); i <= _sheet.LastRowNum; i++)
             {
                 IRow _row = _sheet.GetRow(i);
                 bool _emptyRow = true;
-                
+
                 if(_row == null) continue;
-                
+
                 object[] _itemArray = new object[_row.LastCellNum];
-                
+
                 for(int j = _row.FirstCellNum; j < _row.LastCellNum; j++)
                 {
                     if(_row.GetCell(j) == null) continue;
-                    
+
                     switch(_row.GetCell(j).CellType)
                     {
                         case CellType.Numeric:
@@ -59,16 +61,16 @@
                             {
                                 _itemArray[j] = _row.GetCell(j).NumericCellValue;
                             }
-                            
+
                             break;
-                            
+
                         case CellType.Blank:
                             _itemArray[j] = string.Empty;
                             break;
-                            
+
                         case CellType.Formula:
                             IFormulaEvaluator _eva = null;
-                            
+
                             if(_supportFormula)
                             {
                                 _eva = new XSSFFormulaEvaluator(_hssfworkbook);
@@ -77,7 +79,7 @@
                             {
                                 _eva = new HSSFFormulaEvaluator(_hssfworkbook);
                             }
-                            
+
                             if(_eva.Evaluate(_row.GetCell(j)).CellType == CellType.Numeric)
                             {
                                 _itemArray[j] = _eva.Evaluate(_row.GetCell(j)).NumberValue;
@@ -86,29 +88,29 @@
                             {
                                 _itemArray[j] = _eva.Evaluate(_row.GetCell(j)).StringValue;
                             }
-                            
+
                             break;
-                            
+
                         default:
                             _itemArray[j] = _row.GetCell(j).StringCellValue;
                             break;
                     }
-                    
+
                     if(_itemArray[j] != null && !string.IsNullOrEmpty(_itemArray[j].ToString().Trim()))
                     {
                         _emptyRow = false;
                     }
                 }
-                
+
                 if(!_emptyRow)
                 {
                     _table.Rows.Add(_itemArray);
                 }
             }
-            
+
             return _table;
         }
-        
+
         /// <summary>
         /// 导出到Excel
         /// </summary>
@@ -124,7 +126,7 @@
             .NotNullOrEmpty(title, "EXCEL标题")
             .NotNullOrEmpty(filePath, "EXCEL导出路径")
             .IsFilePath(filePath);
-            
+
             using(FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
             {
                 IWorkbook _workBook = new HSSFWorkbook();
@@ -145,47 +147,47 @@
                 _row.Cells[0].CellStyle = _cellStyle;
                 //处理表格列头
                 _row = _sheet.CreateRow(1);
-                
+
                 for(int i = 0; i < table.Columns.Count; i++)
                 {
                     _row.CreateCell(i).SetCellValue(table.Columns[i].ColumnName);
                     _row.Height = 350;
                     _sheet.AutoSizeColumn(i);
                 }
-                
+
                 //处理数据内容
                 for(int i = 0; i < table.Rows.Count; i++)
                 {
                     _row = _sheet.CreateRow(2 + i);
                     _row.Height = 250;
-                    
+
                     for(int j = 0; j < table.Columns.Count; j++)
                     {
                         _row.CreateCell(j).SetCellValue(table.Rows[i][j].ToString());
                         _sheet.SetColumnWidth(j, 256 * 15);
                     }
                 }
-                
+
                 //写入数据流
                 _workBook.Write(fileStream);
             }
         }
-        
+
         private static void AddDataColumns(ISheet sheet, ushort headIndex, DataTable table)
         {
             IRow _headRow = sheet.GetRow(headIndex);
-            
+
             if(_headRow != null)
             {
                 int _colCount = _headRow.LastCellNum;
-                
+
                 for(int i = 0; i < _colCount; i++)
                 {
                     table.Columns.Add(_headRow.GetCell(i).StringCellValue);
                 }
             }
         }
-        
+
         #endregion Methods
     }
 }
