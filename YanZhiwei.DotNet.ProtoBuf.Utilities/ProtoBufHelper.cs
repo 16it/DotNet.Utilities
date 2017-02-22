fbeb45f239c44e3a932c3594832dfd78bd566550
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-02-22 11:14:53 +0800

    coding

---
+++ YanZhiwei.DotNet.ProtoBuf.Utilities/ProtoBufHelper.cs
@@ -0,0 +1,47 @@
+﻿using ProtoBuf;
+using System.IO;
+using YanZhiwei.DotNet2.Utilities.Operator;
+
+namespace YanZhiwei.DotNet.ProtoBuf.Utilities
+{
+    /// <summary>
+    /// 利用ProtoBuf序列化与反序列化对象
+    /// </summary>
+    public class ProtoBufHelper
+    {
+        /// <summary>
+        /// 序列化二进制流
+        ///<para>https://github.com/mgravell/protobuf-net</para>
+        /// </summary>
+        /// <param name="value">需要序列化对象</param>
+        /// <returns>二进制流</returns>
+        public static byte[] Serialize(object value)
+        {
+            ValidateOperator.Begin().NotNull(value, "需要序列化对象");
+            byte[] _buffer = null;
+            
+            using(MemoryStream stream = new MemoryStream())
+            {
+                Serializer.Serialize(stream, value);
+                _buffer = stream.ToArray();
+            }
+            
+            return _buffer;
+        }
+        
+        /// <summary>
+        /// 序列化成BIN文件
+        /// </summary>
+        /// <param name="value">需要序列化对象</param>
+        /// <param name="path">bin文件存储路径</param>
+        public static void Serialize(object value, string path)
+        {
+            ValidateOperator.Begin().NotNull(value, "需要序列化对象").IsFilePath(path);
+            
+            using(var file = File.Create(path))
+            {
+                Serializer.Serialize(file, value);
+            }
+        }
+    }
+}
---
+++ YanZhiwei.DotNet.ProtoBuf.UtilitiesTests/ProtoBufHelperTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtoBuf;
+using System.Collections.Generic;
+using System.IO;
+
+namespace YanZhiwei.DotNet.ProtoBuf.Utilities.Tests
+{
+    [TestClass()]
+    public class ProtoBufHelperTests
+    {
+        private static List<Person> personList = null;
+        private static string serializeFilePath = @"D:\ProtoBufHelperTests.bin";
+        
+        [TestInitialize]
+        public void Init()
+        {
+            personList = new List<Person>();
+            
+            for(int i = 0; i < 1000; i++)
+            {
+                personList.Add(new Person()
+                {
+                    Name = "churenyouzi",
+                    Addr = "zhuzhou"
+                });
+            }
+        }
+        
+        [TestMethod()]
+        public void SerializeTest()
+        {
+            Assert.IsNotNull(ProtoBufHelper.Serialize(personList));
+            ProtoBufHelper.Serialize(personList, serializeFilePath);
+            bool _actual = File.Exists(serializeFilePath);
+            Assert.IsTrue(_actual);
+        }
+        
+        [TestCleanup]
+        public void End()
+        {
+            if(File.Exists(serializeFilePath))
+                File.Delete(serializeFilePath);
+        }
+    }
+    
+    [ProtoContract]
+    public class Person
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        
+        public string Addr
+        {
+            get;
+            set;
+        }
+    }
+}
