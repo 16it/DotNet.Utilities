3660116b56b0aae00edc1b29652eeab15bb59f72
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-02-24 10:19:01 +0800

    coding

--- YanZhiwei.DotNet4.5.Utilities/Common/EnumerableHelper.cs
+++ YanZhiwei.DotNet4.5.Utilities/Common/EnumerableHelper.cs
@@ -15,13 +15,14 @@ namespace YanZhiwei.DotNet4._5.Utilities.Common
         /// <param name="item">需要分页的数据源</param>
         /// <param name="dataPageSize">每页大小</param>
         /// <param name="dataPageFactory">分页处理委托，参数：每页数据源，总页数，页索引/param>
+        /// <returns>Task</returns>
         public static async Task DataPageProcessAsync<T>(
             IEnumerable<T> item, int dataPageSize,
             Action<IEnumerable<T>, int, int> dataPageFactory) where T : class
         {
             await Task.Run(() =>
             {
-                YanZhiwei.DotNet3._5.Utilities.Common.EnumerableHelper.DataPageProcess<T>(item, dataPageSize, dataPageFactory);
+                DotNet3._5.Utilities.Common.EnumerableHelper.DataPageProcess<T>(item, dataPageSize, dataPageFactory);
             });
         }
     }
