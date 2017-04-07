43436401f4a2aa3d75992cead439dd9de525ffbb
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-04-07 11:16:32 +0800

    coding

---
+++ YanZhiwei.DotNet3.5.Utilities/Cache/ForeverCache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YanZhiwei.DotNet3._5.Utilities.Cache
+{
+    /// <summary>
+    ///永久缓存基类
+    /// </summary>
+    public abstract class ForeverCache<TImplement, TKey, TValue>
+        where TImplement : class
+    {
+    
+    }
+}
