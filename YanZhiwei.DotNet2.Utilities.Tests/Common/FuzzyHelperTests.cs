0376df82c27cb5bf38b2748e51bfad6c78ffefb6
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-04-12 14:23:57 +0800

    coding

---
+++ YanZhiwei.DotNet2.Utilities.Tests/Common/FuzzyHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using YanZhiwei.DotNet2.Utilities.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YanZhiwei.DotNet2.Utilities.Common.Tests
+{
+    [TestClass()]
+    public class FuzzyHelperTests
+    {
+        [TestMethod()]
+        public void FuzzyEmailTest()
+        {
+            string _email = "churenyouzi@outlook.com";
+            string _action = FuzzyHelper.FuzzyEmail(_email);
+            string _expected = "ch*********@outlook.com";
+            Assert.AreEqual(_action, _expected);
+        }
+
+        [TestMethod()]
+        public void FuzzyMobieNumberTest()
+        {
+            string _phoneNumber = "18501600110";
+            string _action = FuzzyHelper.FuzzyMobieNumber(_phoneNumber);
+            string _expected = "185****0110";
+            Assert.AreEqual(_action, _expected);
+        }
+
+        [TestMethod()]
+        public void FuzzyUserNameTest()
+        {
+            string _userName = "朱重八";
+            string _action = FuzzyHelper.FuzzyUserName(_userName);
+            string _expected = "朱***八";
+            Assert.AreEqual(_action, _expected);
+        }
+    }
+}
--- YanZhiwei.DotNet4.Utilities/EventHandle/CustomizeEventPublisher.cs
+++ YanZhiwei.DotNet4.Utilities/EventHandle/CustomizeEventPublisher.cs
@@ -14,60 +14,60 @@ namespace YanZhiwei.DotNet4.Utilities.EventHandle
     {
         private static readonly ThreadLocal<List<CustomizeEventSubscriber<T>>> subscribers = new ThreadLocal<List<CustomizeEventSubscriber<T>>>();
         private static readonly ThreadLocal<bool> publishing = new ThreadLocal<bool> { Value = false };
-        
+
         private static CustomizeEventPublisher<T> instance;
-        
+
         /// <summary>
         /// 初始化
         /// </summary>
         /// <returns>CustomizeEventPublisher</returns>
         public static CustomizeEventPublisher<T> Instance()
         {
-            if(instance != null)
+            if (instance != null)
                 return instance;
-                
+
             CustomizeEventPublisher<T> _eventPublisher = new CustomizeEventPublisher<T>();
             Interlocked.CompareExchange(ref instance, _eventPublisher, null);
             return _eventPublisher;
         }
-        
+
         /// <summary>
         /// 触发事件
         /// </summary>
         /// <param name="aDomainEvent">事件</param>
         public void Publish(T aDomainEvent)
         {
-            if(publishing.Value)
+            if (publishing.Value)
             {
                 return;
             }
-            
+
             try
             {
                 publishing.Value = true;
                 List<CustomizeEventSubscriber<T>> _registeredSubscribers = subscribers.Value;
-                
-                if(_registeredSubscribers != null)
+
+                if (_registeredSubscribers != null)
                 {
                     Type _eventType = aDomainEvent.GetType();
-                    
-                    foreach(var domainEventSubscriber in _registeredSubscribers)
+
+                    foreach (var domainEventSubscriber in _registeredSubscribers)
                     {
-                        if(aDomainEvent.IsRead)
+                        if (aDomainEvent.IsRead)
                             continue;
-                            
+
                         Type _subscribedTo = domainEventSubscriber.SubscribedToEventType();
-                        
-                        if(_subscribedTo == _eventType || _subscribedTo is ICustomizeEvent)
+
+                        if (_subscribedTo == _eventType || _subscribedTo is ICustomizeEvent)
                         {
                             domainEventSubscriber.HandleEvent(aDomainEvent);
                         }
                     }
-                    
+
                     aDomainEvent.Read();
                 }
             }
-            catch(Exception)
+            catch (Exception)
             {
                 throw;
             }
@@ -76,42 +76,42 @@ namespace YanZhiwei.DotNet4.Utilities.EventHandle
                 publishing.Value = false;
             }
         }
-        
+
         /// <summary>
         /// 重置
         /// </summary>
         /// <returns>CustomizeEventPublisher</returns>
         public CustomizeEventPublisher<T> Reset()
         {
-            if(!publishing.Value)
+            if (!publishing.Value)
             {
                 subscribers.Value = null;
             }
-            
+
             return this;
         }
-        
+
         /// <summary>
         /// 增加订阅
         /// </summary>
         /// <param name="aSubscriber">CustomizeEventSubscriber</param>
         public void Subscribe(CustomizeEventSubscriber<T> aSubscriber)
         {
-            if(publishing.Value)
+            if (publishing.Value)
                 return;
-                
+
             List<CustomizeEventSubscriber<T>> _registeredSubscribers = subscribers.Value;
-            
-            if(_registeredSubscribers == null)
+
+            if (_registeredSubscribers == null)
             {
                 _registeredSubscribers = new List<CustomizeEventSubscriber<T>>();
                 subscribers.Value = _registeredSubscribers;
             }
-            
-            if(_registeredSubscribers.Any(ent => ent.SubscribedToEventType().FullName == aSubscriber.SubscribedToEventType().FullName
-                                          && ent.GetType().FullName == aSubscriber.GetType().FullName))  //相同的订阅只接收一次。
+
+            if (_registeredSubscribers.Any(ent => ent.SubscribedToEventType().FullName == aSubscriber.SubscribedToEventType().FullName
+                                           && ent.GetType().FullName == aSubscriber.GetType().FullName))  //相同的订阅只接收一次。
                 return;
-                
+
             _registeredSubscribers.Add(aSubscriber);
         }
     }
