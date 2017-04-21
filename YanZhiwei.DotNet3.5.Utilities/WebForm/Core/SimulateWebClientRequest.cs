c053ed4f79cf523dbe28d1056740a61b05a11405
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-04-21 14:44:32 +0800

    coding

---
+++ YanZhiwei.DotNet3.5.Utilities/WebForm/Core/SimulateWebClientRequest.cs
@@ -0,0 +1,136 @@
+﻿namespace YanZhiwei.DotNet3._5.Utilities.WebForm.Core
+{
+    using System.Collections.Specialized;
+    using System.Text;
+
+    using YanZhiwei.DotNet3._5.Utilities.Common;
+    using YanZhiwei.DotNet3._5.Utilities.Enum;
+    using YanZhiwei.DotNet3._5.Utilities.WebClient;
+
+    /// <summary>
+    /// 利用WebClient模拟Get，Post请求
+    /// </summary>
+    public sealed class SimulateWebClientRequest
+    {
+        #region Methods
+
+        /// <summary>
+        /// 向远程Url Post数据
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="postData">post数据</param>
+        /// <param name="header">Header数据</param>
+        /// <param name="serializationType">post数据以及返回数据序列号处理方式</param>
+        /// <returns>返回数据</returns>
+        public static string Post(string url, object postData, NameValueCollection header, SerializationType serializationType)
+        {
+            string _serializaPostDataString = SerializePostDataObject(postData, serializationType);
+
+            using (CNNWebClient _webClient = new CNNWebClient())
+            {
+                _webClient.Timeout = 300;
+                if (header != null)
+                    _webClient.Headers.Add(header);
+
+                byte[] _responseBuffer = _webClient.UploadData(url, "POST", Encoding.UTF8.GetBytes(_serializaPostDataString));
+
+                return _responseBuffer != null ? Encoding.UTF8.GetString(_responseBuffer) : string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 向远程Url Post数据
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="postData">post数据</param>
+        /// <param name="serializationType">反序列化方式，支持Json,XML</param>
+        /// <returns>反序列化结果</returns>
+        public static T Post<T>(string url, object postData, SerializationType serializationType)
+            where T : class
+        {
+            string _responeString = Post(url, postData, null, serializationType);
+            return DeserializeResponeString<T>(_responeString, serializationType);
+        }
+
+        /// <summary>
+        ///  向远程Url Post数据
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="serializationType">post数据</param>
+        /// <returns></returns>
+        public static T Post<T>(string url, SerializationType serializationType)
+            where T : class
+        {
+            string _responeString = Post(url, new NameValueCollection(), null, serializationType);
+            return DeserializeResponeString<T>(_responeString, serializationType);
+        }
+
+        /// <summary>
+        /// 向远程Url Post数据
+        /// </summary>
+        /// <param name="url">url</param>
+        /// <param name="postData">post数据</param>
+        /// <param name="header">Header数据</param>
+        /// <param name="serializationType">post数据以及返回数据序列号处理方式</param>
+        /// <returns>返回数据</returns>
+        public static T Post<T>(string url, object postData, NameValueCollection header, SerializationType serializationType)
+            where T : class
+        {
+            string _responeString = Post(url, postData, header, serializationType);
+            return DeserializeResponeString<T>(_responeString, serializationType);
+        }
+
+        private static T DeserializeResponeString<T>(string responeString, SerializationType serializationType)
+            where T : class
+        {
+            if (!string.IsNullOrEmpty(responeString))
+            {
+                return serializationType == SerializationType.Xml ? SerializeHelper.XmlDeserialize<T>(responeString) : SerializeHelper.JsonDeserialize<T>(responeString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static string SerializePostDataObject(object postData, SerializationType serializationType)
+        {
+            string _serializaPostDataString = string.Empty;
+            if (postData != null)
+            {
+                if (postData is string)
+                {
+                    _serializaPostDataString = (string)postData;
+                }
+                else
+                {
+                    _serializaPostDataString = serializationType == SerializationType.Xml ? SerializeHelper.XmlSerialize(postData) : SerializeHelper.JsonSerialize(postData);
+                }
+            }
+            return _serializaPostDataString;
+        }
+
+        #endregion Methods
+
+        #region Other
+
+        /*
+         *参考
+         *1. http://www.cnblogs.com/sunkaixuan/p/4992576.html
+
+         *知识
+          1. get方法传输的数据的写法： Request.QueryString["name"];
+          2. post方法传输的数据的写法:Request.Form["name"];
+          3. get和post方法传送数据的代码写法:Request.Params["name"];
+          4. get是从服务器上获取数据，post是向服务器传送数据;
+          5. get是把参数数据队列加到提交表单的ACTION属性所指的URL中，值和表单内各个字段一一对应，在URL中可以看到。post是通过HTTP post机制，将表单内各个字段与其内容放置在HTML HEADER内一起传送到ACTION属性所指的URL地址。用户看不到这个过程。
+          6. 对于get方式，服务器端用Request.QueryString获取变量的值，对于post方式，服务器端用Request.Form获取提交的数据。
+          7. get传送的数据量较小，不能大于2KB。post传送的数据量较大，一般被默认为不受限制。但理论上，IIS4中最大量为80KB，IIS5中为100KB。
+          8. get安全性非常低，post安全性较高。但是执行效率却比Post方法好。
+          9. get方式的安全性较Post方式要差些，包含机密信息的话，建议用Post数据提交方式；
+          10.在做数据查询时，建议用Get方式；而在做数据添加、修改或删除时，建议用Post方式；
+          */
+
+        #endregion Other
+    }
+}
---
+++ YanZhiwei.DotNet3.5.Utilities/WebForm/Core/SimulateWebRequest.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using YanZhiwei.DotNet3._5.Utilities.Common;
+
+namespace YanZhiwei.DotNet3._5.Utilities.WebForm.Core
+{
+    /// <summary>
+    /// 发起模拟Web请求
+    /// </summary>
+    public sealed class SimulateWebRequest
+    {
+        #region Fields
+
+        /// <summary>
+        /// accept
+        /// </summary>
+        private string accept = "*/*";
+
+        /// <summary>
+        /// 是否允许重定向
+        /// </summary>
+        private bool allowAutoRedirect = true;
+
+        /// <summary>
+        /// contentType
+        /// </summary>
+        private string contentType = "application/x-www-form-urlencoded";
+
+        /// <summary>
+        /// 设置cookie
+        /// </summary>
+        private CookieContainer cookie;
+
+        /// <summary>
+        /// 过期时间
+        /// </summary>
+        private int time = 5000;
+
+        #endregion Fields
+
+        /// <summary>
+        /// 设置accept(默认:*/*)
+        /// </summary>
+        /// <param name="accept"></param>
+        public void SetAccept(string accept)
+        {
+            this.accept = accept;
+        }
+
+        /// <summary>
+        /// 设置contentType(默认:application/x-www-form-urlencoded)
+        /// </summary>
+        /// <param name="contentType"></param>
+        public void SetContentType(string contentType)
+        {
+            this.contentType = contentType;
+        }
+
+        /// <summary>
+        /// 设置Cookie
+        /// </summary>
+        /// <param name="cookie"></param>
+        public void SetCookie(CookieContainer cookie)
+        {
+            this.cookie = cookie;
+        }
+
+        /// <summary>
+        /// 是否允许重定向(默认:true)
+        /// </summary>
+        /// <param name="allowAutoRedirect"></param>
+        public void SetIsAllowAutoRedirect(bool allowAutoRedirect)
+        {
+            this.allowAutoRedirect = allowAutoRedirect;
+        }
+
+        /// <summary>
+        /// 设置请求过期时间（单位：毫秒）（默认：5000）
+        /// </summary>
+        /// <param name="time"></param>
+        public void SetTimeOut(int time)
+        {
+            this.time = time;
+        }
+
+        /// <summary>
+        /// 远程证书验证，固定返回true
+        /// </summary>
+        private static bool RemoteCertificateValidate(object sender, X509Certificate cert,
+                X509Chain chain, SslPolicyErrors error)
+        {
+            return true;
+        }
+
+        //注册证书验证回调事件，在请求之前注册
+        private void SetCertificatePolicy()
+        {
+            ServicePointManager.ServerCertificateValidationCallback
+            += RemoteCertificateValidate;
+        }
+
+        /// <summary>
+        /// 发起post请求
+        /// </summary>
+        /// <param name="url">请求URL地址</param>
+        /// <returns>返回结果</returns>
+        public static T HttpPost<T>(string url)
+        {
+            HttpWebRequest _request = System.Net.WebRequest.Create(url) as HttpWebRequest;
+            _request.Method = "POST";
+            _request.Timeout = 30000;
+            _request.AllowAutoRedirect = true;
+            _request.ServicePoint.ConnectionLimit = int.MaxValue;
+            _request.ContentLength = 0;
+
+            using (var response = (HttpWebResponse)_request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    string _responeString = reader.ReadToEnd();
+                    return SerializeHelper.JsonDeserialize<T>(_responeString);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获得响应中的图像
+        /// </summary>
+        /// <param name="url">链接</param>
+        /// <returns>若发生异常则返回NULL</returns>
+        public Stream GetResponseImage(string url)
+        {
+            Stream _responseStream = null;
+
+            try
+            {
+                HttpWebRequest _request = (HttpWebRequest)System.Net.WebRequest.Create(url);
+                _request.KeepAlive = true;
+                _request.Method = "GET";
+                _request.AllowAutoRedirect = allowAutoRedirect;
+                _request.CookieContainer = cookie;
+                _request.ContentType = this.contentType;
+                _request.Accept = this.accept;
+                _request.Timeout = time;
+                Encoding _encoding = Encoding.GetEncoding("UTF-8");
+                this.SetCertificatePolicy();
+                HttpWebResponse _response = (HttpWebResponse)_request.GetResponse();
+                _responseStream = _response.GetResponseStream();
+                return _responseStream;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// POST文件
+        /// </summary>
+        /// <param name="url">链接</param>
+        /// <param name="file">文件路径</param>
+        /// <param name="postData"></param>
+        /// <returns>Html</returns>
+        public string HttpUploadFile(string url, string file, NameValueCollection postData)
+        {
+            return HttpUploadFile(url, file, postData, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// POST文件
+        /// </summary>
+        /// <param name="url">链接</param>
+        /// <param name="file">文件路径</param>
+        /// <param name="postData">参数</param>
+        /// <param name="encoding">编码</param>
+        /// <returns></returns>
+        public string HttpUploadFile(string url, string file, NameValueCollection postData, Encoding encoding)
+        {
+            return HttpUploadFile(url, new string[] { file }, postData, encoding);
+        }
+
+        /// <summary>
+        /// POST文件
+        /// </summary>
+        /// <param name="url">链接</param>
+        /// <param name="files">文件路径</param>
+        /// <param name="postData">参数</param>
+        /// <returns>Html</returns>
+        public string HttpUploadFile(string url, string[] files, NameValueCollection postData)
+        {
+            return HttpUploadFile(url, files, postData, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// POST文件
+        /// </summary>
+        /// <param name="url">链接</param>
+        /// <param name="files">文件路径</param>
+        /// <param name="postData">参数</param>
+        /// <param name="encoding">编码</param>
+        /// <returns></returns>
+        public string HttpUploadFile(string url, string[] files, NameValueCollection postData, Encoding encoding)
+        {
+            string _boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] _boundarybytes = Encoding.ASCII.GetBytes("\r\n--" + _boundary + "\r\n");
+            byte[] _endbytes = Encoding.ASCII.GetBytes("\r\n--" + _boundary + "--\r\n");
+            //1.HttpWebRequest
+            HttpWebRequest _request = (HttpWebRequest)System.Net.WebRequest.Create(url);
+            _request.ContentType = "multipart/form-data; boundary=" + _boundary;
+            _request.Method = "POST";
+            _request.KeepAlive = true;
+            _request.Accept = this.accept;
+            _request.Timeout = this.time;
+            _request.AllowAutoRedirect = this.allowAutoRedirect;
+
+            if (cookie != null)
+                _request.CookieContainer = cookie;
+
+            _request.Credentials = CredentialCache.DefaultCredentials;
+            using (Stream stream = _request.GetRequestStream())
+            {
+                //1.1 key/value
+                string _formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+
+                if (postData != null)
+                {
+                    foreach (string key in postData.Keys)
+                    {
+                        stream.Write(_boundarybytes, 0, _boundarybytes.Length);
+                        string formitem = string.Format(_formdataTemplate, key, postData[key]);
+                        byte[] formitembytes = encoding.GetBytes(formitem);
+                        stream.Write(formitembytes, 0, formitembytes.Length);
+                    }
+                }
+
+                //1.2 file
+                string _headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: application/octet-stream\r\n\r\n";
+                byte[] _buffer = new byte[4096];
+                int _bytesRead = 0;
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    stream.Write(_boundarybytes, 0, _boundarybytes.Length);
+                    string _header = string.Format(_headerTemplate, "file" + i, Path.GetFileName(files[i]));
+                    byte[] _headerbytes = encoding.GetBytes(_header);
+                    stream.Write(_headerbytes, 0, _headerbytes.Length);
+                    using (FileStream fileStream = new FileStream(files[i], FileMode.Open, FileAccess.Read))
+                    {
+                        while ((_bytesRead = fileStream.Read(_buffer, 0, _buffer.Length)) != 0)
+                        {
+                            stream.Write(_buffer, 0, _bytesRead);
+                        }
+                    }
+                }
+
+                //1.3 form end
+                stream.Write(_endbytes, 0, _endbytes.Length);
+            }
+            //2.WebResponse
+            HttpWebResponse _response = (HttpWebResponse)_request.GetResponse();
+            using (StreamReader stream = new StreamReader(_response.GetResponseStream()))
+            {
+                return stream.ReadToEnd();
+            }
+        }
+    }
+}
