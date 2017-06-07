b689b9055e59e8007ab5091e55fbef16f9d156af
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-06-07 11:03:40 +0800

    coding

---
+++ YanZhiwei.DotNet.Core.Config/Model/AdminMenuConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace YanZhiwei.DotNet.Core.Config.Model
+{
+    /// <summary>
+    /// 后台菜单配置
+    /// </summary>
+    /// <seealso cref="YanZhiwei.DotNet.Core.Config.Model.ConfigFileBase" />
+    [Serializable]
+    public class AdminMenuConfig : ConfigFileBase
+    {
+        /// <summary>
+        /// 默认构造函数
+        /// </summary>
+        public AdminMenuConfig()
+        {
+        }
+        
+        /// <summary>
+        /// 后台菜单分类
+        /// </summary>
+        public AdminMenuGroup[] AdminMenuGroups
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ YanZhiwei.DotNet.Core.Config/Model/AdminMenuGroup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace YanZhiwei.DotNet.Core.Config.Model
+{
+    /// <summary>
+    /// 后台菜单分类
+    /// </summary>
+    [Serializable]
+    public class AdminMenuGroup
+    {
+        /// <summary>
+        /// 后台菜单分类子集合
+        /// </summary>
+        public List<AdminMenu> AdminMenuArray
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// Id
+        /// </summary>
+        [XmlAttribute("id")]
+        public string Id
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        [XmlAttribute("name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        ///链接
+        /// </summary>
+        [XmlAttribute("url")]
+        public string Url
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// 菜单图标
+        /// </summary>
+        [XmlAttribute("icon")]
+        public string Icon
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// 访问权限
+        /// </summary>
+        [XmlAttribute("permission")]
+        public string Permission
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        ///菜单信息
+        /// </summary>
+        [XmlAttribute("info")]
+        public string Info
+        {
+            get;
+            set;
+        }
+    }
+}
