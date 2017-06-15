905a224eea851f23de56d43f775ddc7f3aed36e6
言志伟 <Yan.Zhiwei@hotmail.com>
2017-06-15 23:24:24 +0800

    coding

--- YanZhiwei.DotNet.Core.WebApi/Filter/WebApiOutputCacheAttribute.cs
+++ YanZhiwei.DotNet.Core.WebApi/Filter/WebApiOutputCacheAttribute.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Caching;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
@@ -14,24 +12,22 @@ namespace YanZhiwei.DotNet.Core.WebApi.Filter
     /// <summary>
     /// Action Filter to cache responses
     /// </summary>
-    public class WebApiOutputCacheAttribute : ActionFilterAttribute
+    public abstract class WebApiOutputCacheAttribute : ActionFilterAttribute
     {
         /// <summary>
         /// 缓存时间【秒】
         /// </summary>
-        private int cacheSeconds;
+        public int CacheSeconds;
 
         /// <summary>
         /// 是否启用缓存
         /// </summary>
-        private bool cacheEnabled = false;
+        private bool cacheEnabled = true;
 
         /// <summary>
         /// 缓存取决于访问令牌
         /// </summary>
         private readonly bool dependsOnIdentity;
-
-
 
         // cache repository
         private static readonly ObjectCache WebApiCache = MemoryCache.Default;
@@ -68,9 +64,8 @@ namespace YanZhiwei.DotNet.Core.WebApi.Filter
             this.dependsOnIdentity = dependsOnIdentity;
             this.invalidateCache = invalidateCache;
 
-            ReadConfig();
+            //   ReadConfig();
         }
-
 
         /// <summary>
         /// Called when [action executing].
@@ -115,9 +110,11 @@ namespace YanZhiwei.DotNet.Core.WebApi.Filter
                      });
 
             if (dependsOnIdentity)
-                _cachekey = _cachekey.Insert(0, identityToken);
+                _cachekey = _cachekey.Insert(0, GetIdentityToken(actionContext));
             return _cachekey;
         }
+
+        public abstract string GetIdentityToken(HttpActionContext actionContext);
 
         /// <summary>
         /// Called when [action executed].
@@ -138,12 +135,12 @@ namespace YanZhiwei.DotNet.Core.WebApi.Filter
 
                             if (WebApiCache.Contains(_cachekey))
                             {
-                                WebApiCache.Set(_cachekey, body, DateTime.Now.AddSeconds(cacheSeconds));
+                                WebApiCache.Set(_cachekey, body, DateTime.Now.AddSeconds(CacheSeconds));
                                 WebApiCache.Set(_cachekey + ":response-ct", actionExecutedContext.Response.Content.Headers.ContentType, DateTime.Now.AddSeconds(_timespan));
                             }
                             else
                             {
-                                WebApiCache.Add(_cachekey, body, DateTime.Now.AddSeconds(cacheSeconds));
+                                WebApiCache.Add(_cachekey, body, DateTime.Now.AddSeconds(CacheSeconds));
                                 WebApiCache.Add(_cachekey + ":response-ct", actionExecutedContext.Response.Content.Headers.ContentType, DateTime.Now.AddSeconds(_timespan));
                             }
                         }
@@ -176,26 +173,23 @@ namespace YanZhiwei.DotNet.Core.WebApi.Filter
             }
         }
 
-        private void ReadConfig()
-        {
-            if (!Boolean.TryParse(WebConfigurationManager.AppSettings["CacheEnabled"], out cacheEnabled))
-                cacheEnabled = false;
+        //private void ReadConfig()
+        //{
+        //    if (!Boolean.TryParse(WebConfigurationManager.AppSettings["CacheEnabled"], out cacheEnabled))
+        //        cacheEnabled = false;
 
-            if (!Int32.TryParse(WebConfigurationManager.AppSettings["CacheTimespan"], out _timespan))
-                _timespan = 1800; // seconds
-        }
+        //    if (!Int32.TryParse(WebConfigurationManager.AppSettings["CacheTimespan"], out _timespan))
+        //        _timespan = 1800; // seconds
+        //}
 
         private bool IsCacheable(HttpActionContext ac)
         {
-            if (_timespan > 0)
+            if (CacheSeconds > 0)
             {
                 if (ac.Request.Method == HttpMethod.Get || ac.Request.Method == HttpMethod.Post)
                     return true;
             }
-            else
-            {
-                throw new InvalidOperationException("Wrong Arguments");
-            }
+
             return false;
         }
     }
--- YanZhiwei.DotNet4.Utilities/Cache/MemoryCacheManager.cs
+++ YanZhiwei.DotNet4.Utilities/Cache/MemoryCacheManager.cs
@@ -14,46 +14,59 @@ namespace YanZhiwei.DotNet4.Utilities.Cache
     public class MemoryCacheManager
     {
         #region Fields
-        
-        private readonly ObjectCache objectCache;
-        
+
+        private static readonly ObjectCache objectCache;
+
         #endregion Fields
-        
+
         #region Constructors
-        
+
         /// <summary>
         /// 默认构造函数
         /// </summary>
-        public MemoryCacheManager()
+        static MemoryCacheManager()
         {
             objectCache = MemoryCache.Default;
         }
-        
+
         #endregion Constructors
-        
+
         #region Methods
-        
+
         /// <summary>
         /// 清除缓存
         /// </summary>
         /// <param name="keyRegex">正则表达式</param>
-        public void Clear(string keyRegex)
+        public static void Clear(string keyRegex)
         {
             List<string> _keys = new List<string>();
             List<string> _cacheKeys = objectCache.Select(m => m.Key).ToList();
-            
-            foreach(string key in _cacheKeys)
+
+            foreach (string key in _cacheKeys)
             {
-                if(Regex.IsMatch(key, keyRegex, RegexOptions.IgnoreCase))
+                if (Regex.IsMatch(key, keyRegex, RegexOptions.IgnoreCase))
                     _keys.Add(key);
             }
-            
-            for(int i = 0; i < _keys.Count; i++)
+
+            for (int i = 0; i < _keys.Count; i++)
             {
                 objectCache.Remove(_keys[i]);
             }
         }
-        
+
+        /// <summary>
+        /// 缓存是否存在
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <returns>
+        ///   缓存是否存在
+        /// </returns>
+        public static bool Contains(string key)
+        {
+            CheckedParamter(key);
+            string _cacheKey = GetCacheKey(key);
+            return objectCache.Contains(_cacheKey);
+        }
         /// <summary>
         /// 以键取值
         /// </summary>
@@ -61,117 +74,117 @@ namespace YanZhiwei.DotNet4.Utilities.Cache
         /// <returns>
         /// 值
         /// </returns>
-        public object Get(string key)
+        public static object Get(string key)
         {
             CheckedParamter(key);
             string _cacheKey = GetCacheKey(key);
             object _value = objectCache.Get(_cacheKey);
-            
-            if(_value == null)
+
+            if (_value == null)
             {
                 return null;
             }
-            
+
             DictionaryEntry _entry = (DictionaryEntry)_value;
-            
-            if(!key.Equals(_entry.Key))
+
+            if (!key.Equals(_entry.Key))
             {
                 return null;
             }
-            
+
             return _entry.Value;
         }
-        
+
         /// <summary>
         /// 从缓存中获取强类型数据
         /// </summary>
         /// <typeparam name="T">数据类型</typeparam>
         /// <param name="key">缓存键</param>
         /// <returns>获取的强类型数据</returns>
-        public T Get<T>(string key)
+        public static T Get<T>(string key)
         {
             return (T)Get(key);
         }
-        
+
         /// <summary>
         /// 移除缓存
         /// </summary>
         /// <param name="key">键</param>
-        public void Remove(string key)
+        public static void Remove(string key)
         {
             CheckedParamter(key);
             string _cacheKey = GetCacheKey(key);
             objectCache.Remove(_cacheKey);
         }
-        
+
         /// <summary>
         /// 设置缓存
         /// </summary>
         /// <param name="key">键</param>
         /// <param name="value">值</param>
-        /// <param name="minutes">分钟</param>
+        /// <param name="seconds">秒</param>
         /// <param name="isAbsoluteExpiration">是否绝对时间</param>
         /// <param name="updateCallback">在即将从缓存中移除某个缓存项时将调用该方法</param>
         /// <param name="removedCallback">在从缓存中移除某个缓存项后将调用该方法</param>
-        public void Set(string key, object value, int minutes, bool isAbsoluteExpiration, CacheEntryUpdateCallback updateCallback, CacheEntryRemovedCallback removedCallback)
+        public static void Set(string key, object value, int seconds, bool isAbsoluteExpiration, CacheEntryUpdateCallback updateCallback, CacheEntryRemovedCallback removedCallback)
         {
             CheckedParamter(key, value);
             string _cacheKey = GetCacheKey(key);
             DictionaryEntry _entry = new DictionaryEntry(key, value);
             CacheItemPolicy _policy = null;
-            
-            if(isAbsoluteExpiration)
+
+            if (isAbsoluteExpiration)
             {
                 _policy = new CacheItemPolicy()
                 {
-                    AbsoluteExpiration = DateTime.Now.AddMinutes(minutes)
+                    AbsoluteExpiration = DateTime.Now.AddSeconds(seconds)
                 };
             }
-            
+
             else
             {
                 _policy = new CacheItemPolicy()
                 {
-                    SlidingExpiration = TimeSpan.FromMinutes(minutes)
+                    SlidingExpiration = TimeSpan.FromSeconds(seconds)
                 };
             }
-            
-            if(updateCallback != null)
+
+            if (updateCallback != null)
                 _policy.UpdateCallback = updateCallback;
-                
-            if(removedCallback != null)
+
+            if (removedCallback != null)
                 _policy.RemovedCallback = removedCallback;
-                
+
             objectCache.Set(_cacheKey, _entry, _policy);
         }
-        
+
         /// <summary>
         /// 设置缓存
         /// </summary>
         /// <param name="key">键</param>
         /// <param name="value">值</param>
-        /// <param name="minutes">分钟</param>
+        /// <param name="seconds">秒</param>
         /// <param name="isAbsoluteExpiration">是否绝对时间</param>
-        public void Set(string key, object value, int minutes, bool isAbsoluteExpiration)
+        public static void Set(string key, object value, int seconds, bool isAbsoluteExpiration)
         {
-            Set(key, value, minutes, isAbsoluteExpiration, null, null);
+            Set(key, value, seconds, isAbsoluteExpiration, null, null);
         }
-        
-        private void CheckedParamter(string key, object value)
+
+        private static void CheckedParamter(string key, object value)
         {
             ValidateOperator.Begin().NotNullOrEmpty(key, "缓存键").NotNull(value, "缓存数据");
         }
-        
-        private void CheckedParamter(string key)
+
+        private static void CheckedParamter(string key)
         {
             ValidateOperator.Begin().NotNullOrEmpty(key, "缓存键");
         }
-        
-        private string GetCacheKey(string key)
+
+        private static string GetCacheKey(string key)
         {
             return string.Concat(string.Empty, ":", key, "@", key.GetHashCode());
         }
-        
+
         #endregion Methods
     }
 }
