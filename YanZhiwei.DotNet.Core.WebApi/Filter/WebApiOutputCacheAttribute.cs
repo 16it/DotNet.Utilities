0f11c3212959049dce599f6d6136fbfac3beed66
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-06-15 18:22:46 +0800

    coding

---
+++ YanZhiwei.DotNet.Core.WebApi/Filter/WebApiOutputCacheAttribute.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace YanZhiwei.DotNet.Core.WebApi.Filter
+{
+    /// <summary>
+    /// Action Filter to cache responses
+    /// </summary>
+    public class WebApiOutputCacheAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// 缓存时间【秒】
+        /// </summary>
+        private int cacheSeconds;
+
+        /// <summary>
+        /// 是否启用缓存
+        /// </summary>
+        private bool cacheEnabled = false;
+
+        /// <summary>
+        /// 缓存取决于访问令牌
+        /// </summary>
+        private readonly bool dependsOnIdentity;
+
+
+
+        // cache repository
+        private static readonly ObjectCache WebApiCache = MemoryCache.Default;
+
+        /// <summary>
+        /// 无效缓存，用于重置缓存
+        /// </summary>
+        private readonly bool invalidateCache;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public WebApiOutputCacheAttribute()
+            : this(false)
+        {
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="dependsOnIdentity">缓存取决于访问令牌</param>
+        public WebApiOutputCacheAttribute(bool dependsOnIdentity)
+            : this(dependsOnIdentity, false)
+        {
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="dependsOnIdentity">缓存取决于访问令牌</param>
+        /// <param name="invalidateCache">无效缓存，用于重置缓存</param>
+        public WebApiOutputCacheAttribute(bool dependsOnIdentity, bool invalidateCache)
+        {
+            this.dependsOnIdentity = dependsOnIdentity;
+            this.invalidateCache = invalidateCache;
+
+            ReadConfig();
+        }
+
+
+        /// <summary>
+        /// Called when [action executing].
+        /// </summary>
+        /// <param name="actionContext">The filter context.</param>
+        /// <exception cref="System.ArgumentNullException">actionContext</exception>
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (cacheEnabled)
+            {
+                if (actionContext != null)
+                {
+                    if (IsCacheable(actionContext))
+                    {
+                        string _cachekey = CreateCacheKey(actionContext);
+
+                        if (WebApiCache.Contains(_cachekey))
+                        {
+                            string _cacheStrinig = (string)WebApiCache.Get(_cachekey);
+                            if (_cacheStrinig != null)
+                            {
+                                actionContext.Response = actionContext.Request.CreateResponse();
+                                actionContext.Response.Content = new StringContent(_cacheStrinig);
+                                MediaTypeHeaderValue _contenttype = (MediaTypeHeaderValue)WebApiCache.Get(_cachekey + ":response-ct");
+                                if (_contenttype == null)
+                                    _contenttype = new MediaTypeHeaderValue(_cachekey.Split(':')[1]);
+                                actionContext.Response.Content.Headers.ContentType = _contenttype;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private string CreateCacheKey(HttpActionContext actionContext)
+        {
+            string _cachekey = string.Join(":", new string[]
+                     {
+                            actionContext.Request.RequestUri.OriginalString,
+                            actionContext.Request.Headers.Accept.FirstOrDefault().ToString(),
+                     });
+
+            if (dependsOnIdentity)
+                _cachekey = _cachekey.Insert(0, identityToken);
+            return _cachekey;
+        }
+
+        /// <summary>
+        /// Called when [action executed].
+        /// </summary>
+        /// <param name="actionExecutedContext">The filter context.</param>
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            try
+            {
+                if (cacheEnabled)
+                {
+                    if (WebApiCache != null)
+                    {
+                        string _cachekey = CreateCacheKey(actionExecutedContext.ActionContext);
+                        if (actionExecutedContext.Response != null && actionExecutedContext.Response.Content != null)
+                        {
+                            string body = actionExecutedContext.Response.Content.ReadAsStringAsync().Result;
+
+                            if (WebApiCache.Contains(_cachekey))
+                            {
+                                WebApiCache.Set(_cachekey, body, DateTime.Now.AddSeconds(cacheSeconds));
+                                WebApiCache.Set(_cachekey + ":response-ct", actionExecutedContext.Response.Content.Headers.ContentType, DateTime.Now.AddSeconds(_timespan));
+                            }
+                            else
+                            {
+                                WebApiCache.Add(_cachekey, body, DateTime.Now.AddSeconds(cacheSeconds));
+                                WebApiCache.Add(_cachekey + ":response-ct", actionExecutedContext.Response.Content.Headers.ContentType, DateTime.Now.AddSeconds(_timespan));
+                            }
+                        }
+                    }
+                }
+
+                if (invalidateCache)
+                {
+                    CleanCache();
+                }
+            }
+            catch (Exception ex)
+            {
+                //TraceManager.TraceError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Removes all items from the cache
+        /// </summary>
+        private static void CleanCache()
+        {
+            if (WebApiCache != null)
+            {
+                List<string> keyList = WebApiCache.Select(w => w.Key).ToList();
+                foreach (string key in keyList)
+                {
+                    WebApiCache.Remove(key);
+                }
+            }
+        }
+
+        private void ReadConfig()
+        {
+            if (!Boolean.TryParse(WebConfigurationManager.AppSettings["CacheEnabled"], out cacheEnabled))
+                cacheEnabled = false;
+
+            if (!Int32.TryParse(WebConfigurationManager.AppSettings["CacheTimespan"], out _timespan))
+                _timespan = 1800; // seconds
+        }
+
+        private bool IsCacheable(HttpActionContext ac)
+        {
+            if (_timespan > 0)
+            {
+                if (ac.Request.Method == HttpMethod.Get || ac.Request.Method == HttpMethod.Post)
+                    return true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Wrong Arguments");
+            }
+            return false;
+        }
+    }
+}
---
+++ YanZhiwei.DotNet4.Utilities/Cache/MemoryCacheManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text.RegularExpressions;
+using YanZhiwei.DotNet2.Utilities.Operator;
+
+namespace YanZhiwei.DotNet4.Utilities.Cache
+{
+    public class MemoryCacheManager
+    {
+        #region Fields
+
+        private readonly ObjectCache objectCache;
+
+        #endregion Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// 默认构造函数
+        /// </summary>
+        public MemoryCacheManager()
+        {
+            objectCache = MemoryCache.Default;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// 清除缓存
+        /// </summary>
+        /// <param name="keyRegex">正则表达式</param>
+        public virtual void Clear(string keyRegex)
+        {
+            List<string> _keys = new List<string>();
+            List<string> _cacheKeys = objectCache.Select(m => m.Key).ToList();
+
+            foreach (string key in _cacheKeys)
+            {
+                if (Regex.IsMatch(key, keyRegex, RegexOptions.IgnoreCase))
+                    _keys.Add(key);
+            }
+
+            for (int i = 0; i < _keys.Count; i++)
+            {
+                objectCache.Remove(_keys[i]);
+            }
+        }
+
+        /// <summary>
+        /// 以键取值
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <returns>
+        /// 值
+        /// </returns>
+        public virtual object Get(string key)
+        {
+            CheckedParamter(key);
+            string _cacheKey = GetCacheKey(key);
+            object _value = objectCache.Get(_cacheKey);
+
+            if (_value == null)
+            {
+                return null;
+            }
+
+            DictionaryEntry _entry = (DictionaryEntry)_value;
+
+            if (!key.Equals(_entry.Key))
+            {
+                return null;
+            }
+
+            return _entry.Value;
+        }
+
+        /// <summary>
+        /// 从缓存中获取强类型数据
+        /// </summary>
+        /// <typeparam name="T">数据类型</typeparam>
+        /// <param name="key">缓存键</param>
+        /// <returns>获取的强类型数据</returns>
+        public virtual T Get<T>(string key)
+        {
+            return (T)Get(key);
+        }
+
+        /// <summary>
+        /// 移除缓存
+        /// </summary>
+        /// <param name="key">键</param>
+        public virtual void Remove(string key)
+        {
+            CheckedParamter(key);
+            string _cacheKey = GetCacheKey(key);
+            objectCache.Remove(_cacheKey);
+        }
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        /// <param name="minutes">分钟</param>
+        /// <param name="isAbsoluteExpiration">是否绝对时间</param>
+        /// <param name="onRemoveFacotry">在即将从缓存中移除某个缓存项时将调用该方法</param>
+        public virtual void Set(string key, object value, int minutes, bool isAbsoluteExpiration, CacheEntryUpdateCallback updateCallback)
+        {
+            CheckedParamter(key, value);
+            string _cacheKey = GetCacheKey(key);
+            DictionaryEntry _entry = new DictionaryEntry(key, value);
+            CacheItemPolicy _policy = null;
+
+            if (isAbsoluteExpiration)
+            {
+                _policy = new CacheItemPolicy()
+                {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(minutes)
+                };
+            }
+            else
+            {
+                _policy = new CacheItemPolicy()
+                {
+                    SlidingExpiration = TimeSpan.FromMinutes(minutes)
+                };
+            }
+            if (updateCallback != null)
+                _policy.UpdateCallback = updateCallback;
+
+            objectCache.Set(_cacheKey, _entry, _policy);
+        }
+
+        private void CheckedParamter(string key, object value)
+        {
+            ValidateOperator.Begin().NotNullOrEmpty(key, "缓存键").NotNull(value, "缓存数据");
+        }
+
+        private void CheckedParamter(string key)
+        {
+            ValidateOperator.Begin().NotNullOrEmpty(key, "缓存键");
+        }
+
+        private string GetCacheKey(string key)
+        {
+            return string.Concat(string.Empty, ":", key, "@", key.GetHashCode());
+        }
+
+        #endregion Methods
+    }
+}
