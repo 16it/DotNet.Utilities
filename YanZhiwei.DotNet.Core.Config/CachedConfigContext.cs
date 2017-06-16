d263de7c9daba430c8a7eaef88a360e8b4f8f0ea
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-06-16 12:39:15 +0800

    coding

--- YanZhiwei.DotNet.Core.Config/CachedConfigContext.cs
+++ YanZhiwei.DotNet.Core.Config/CachedConfigContext.cs
@@ -4,14 +4,14 @@
     using YanZhiwei.DotNet.Core.Config.Model;
     using YanZhiwei.DotNet2.Utilities.DesignPattern;
     using YanZhiwei.DotNet2.Utilities.WebForm.Core;
-    
+
     /// <summary>
     /// CachedConfigContext
     /// </summary>
     public sealed class CachedConfigContext : ConfigContext
     {
         #region Properties
-        
+
         /// <summary>
         /// 单例对象
         /// </summary>
@@ -22,18 +22,7 @@
                 return Singleton<CachedConfigContext>.Instance;
             }
         }
-        
-        /// <summary>
-        /// WEB API 用户令牌验证配置项
-        /// </summary>
-        public AuthWebApiConfig AuthWebApiConfig
-        {
-            get
-            {
-                return this.Get<AuthWebApiConfig>();
-            }
-        }
-        
+
         /// <summary>
         /// 缓存配置项
         /// </summary>
@@ -44,7 +33,7 @@
                 return this.Get<CacheConfig>();
             }
         }
-        
+
         /// <summary>
         /// 文件下载配置项
         /// </summary>
@@ -55,7 +44,7 @@
                 return this.Get<DownloadConfig>();
             }
         }
-        
+
         /// <summary>
         /// 文件上传配置项
         /// </summary>
@@ -66,7 +55,7 @@
                 return this.Get<UploadConfig>();
             }
         }
-        
+
         /// <summary>
         /// 系统配置
         /// </summary>
@@ -77,7 +66,7 @@
                 return this.Get<SystemConfig>();
             }
         }
-        
+
         /// <summary>
         /// 网站配置
         /// </summary>
@@ -88,7 +77,7 @@
                 return this.Get<SettingConfig>();
             }
         }
-        
+
         /// <summary>
         /// 后台菜单
         /// </summary>
@@ -99,7 +88,7 @@
                 return this.Get<AdminMenuConfig>();
             }
         }
-        
+
         /// <summary>
         /// WebApi缓存配置
         /// </summary>
@@ -110,10 +99,11 @@
                 return this.Get<WebApiOutputCacheConfig>();
             }
         }
+
         #endregion Properties
-        
+
         #region Methods
-        
+
         /// <summary>
         /// 重写基类的取配置，加入缓存机制
         /// </summary>
@@ -122,12 +112,11 @@
             string _fileName = this.GetConfigFileName<T>(index),
                    _key = "ConfigFile_" + _fileName;
             object _content = CacheManger.Get(_key);
-            
-            if(_content != null)
+
+            if (_content != null)
             {
                 return (T)_content;
             }
-            
             else
             {
                 T _value = base.Get<T>(index);
@@ -135,7 +124,7 @@
                 return _value;
             }
         }
-        
+
         #endregion Methods
     }
 }
