10b6ef0f0963ac92e3924945a6c38ff75db087ba
言志伟 <Yan.Zhiwei@hotmail.com>
2017-07-12 23:45:20 +0800

    coding

---
+++ YanZhiwei.DotNet.Core.WebApi/Model/ValidationFailedResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.ModelBinding;
+using YanZhiwei.DotNet2.Utilities.Result;
+
+namespace YanZhiwei.DotNet.Core.WebApi.Model
+{
+    /// <summary>
+    /// 验证失败结果
+    /// </summary>
+    [Serializable]
+    public class ValidationFailedResult : BasicResult<List<ValidationError>>
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="statusCode">响应状态码</param>
+        /// <param name="message">附加错误信息</param>
+        /// <param name="modelState">ModelStateDictionary</param>
+        public ValidationFailedResult(int statusCode, string message, ModelStateDictionary modelState)
+        {
+            base.Data = modelState.Keys
+                        .SelectMany(key => modelState[key].Errors.Select(x => new ValidationError(key, x.ErrorMessage)))
+                        .ToList();
+            base.Message = message;
+            this.StatusCode = statusCode;
+        }
+
+        /// <summary>
+        /// 响应状态码
+        /// </summary>
+        public readonly int StatusCode;
+    }
+}
