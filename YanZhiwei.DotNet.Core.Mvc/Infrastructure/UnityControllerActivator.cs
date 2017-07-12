e273f6a603a77c994fcb6bd1001acf8e68ad8b11
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-07-12 11:21:04 +0800

    coding

---
+++ YanZhiwei.DotNet.Core.Mvc/Infrastructure/UnityControllerActivator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace YanZhiwei.DotNet.Core.Mvc.Infrastructure
+{
+    /// <summary>
+    /// UnityControllerActivator
+    /// </summary>
+    /// <seealso cref="System.Web.Mvc.IControllerActivator" />
+    public sealed class UnityControllerActivator : IControllerActivator
+    {
+        private readonly IUnityContainer unityContainer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnityControllerActivator"/> class.
+        /// </summary>
+        /// <param name="container">IUnityContainer</param>
+        public UnityControllerActivator(IUnityContainer container)
+        {
+            unityContainer = container;
+        }
+
+        /// <summary>
+        /// 在类中实现时创建控制器。
+        /// </summary>
+        /// <param name="requestContext">请求上下文。</param>
+        /// <param name="controllerType">控制器类型。</param>
+        /// <returns>
+        /// 创建的控制器。
+        /// </returns>
+        public IController Create(RequestContext requestContext, Type controllerType)
+        {
+            return (IController)unityContainer.Resolve(controllerType);
+        }
+    }
+}
---
+++ YanZhiwei.DotNet.Core.SignalR/Infrastructure/UnityHubActivator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Practices.Unity;
+
+namespace YanZhiwei.DotNet.Core.SignalR.Infrastructure
+{
+    /// <summary>
+    /// UnityHubActivator
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNet.SignalR.Hubs.IHubActivator" />
+    public sealed class UnityHubActivator : IHubActivator
+    {
+        private readonly IUnityContainer _container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnityHubActivator"/> class.
+        /// </summary>
+        /// <param name="container">IUnityContainer</param>
+        public UnityHubActivator(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        /// <summary>
+        /// Creates the specified descriptor.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns></returns>
+        public IHub Create(HubDescriptor descriptor)
+        {
+            var hubType = descriptor.HubType;
+            return hubType != null ? _container.Resolve(hubType) as IHub : null;
+        }
+    }
+}
