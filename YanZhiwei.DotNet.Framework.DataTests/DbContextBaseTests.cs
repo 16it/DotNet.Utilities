af3631fee0fa24eba8800152b49ba94105067cc4
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-07-21 09:34:27 +0800

    coding

--- YanZhiwei.DotNet.Framework.DataTests/DbContextBaseTests.cs
+++ YanZhiwei.DotNet.Framework.DataTests/DbContextBaseTests.cs
@@ -19,7 +19,7 @@ namespace YanZhiwei.DotNet.Framework.Data.Tests
             {
                 Address _fined = dbContext.Find<Address>(22);
                 dbContext.Delete<Address>(_fined);
-                _fined = dbContext.Find<Address>(3);
+                _fined = dbContext.Find<Address>(22);
                 Assert.IsNull(_fined);
             }
         }
@@ -61,10 +61,9 @@ namespace YanZhiwei.DotNet.Framework.Data.Tests
             using(var dbContext = new AdventureWorks2014DbContext())
             {
                 Address _item = new Address();
-                _item.ID = 1;
                 _item.PostalCode = "78019";
                 _item.AddressLine1 = "Tianxin";
-                _item.City = "Zhuzhou";
+                _item.City = "Changsha";
                 _item.StateProvinceID = 78;
                 _item.PostalCode = "72229";
                 _item.rowguid = Guid.NewGuid();
@@ -105,6 +104,81 @@ namespace YanZhiwei.DotNet.Framework.Data.Tests
                 dbContext.Update<Address>(_actual);
                 _actual = dbContext.Find<Address>(32529);
                 Assert.AreEqual(_actual.AddressLine1, _expect);
+            }
+        }
+        
+        [TestMethod()]
+        public void CommitTest()
+        {
+            using(var dbContext = new AdventureWorks2014DbContext())
+            {
+                dbContext.BeginTransaction();
+                
+                try
+                {
+                    Address _item = new Address();
+                    _item.PostalCode = "78019";
+                    _item.AddressLine1 = "Tianxin";
+                    _item.City = "Xiangtan";
+                    _item.StateProvinceID = 78;
+                    _item.PostalCode = "72228";
+                    _item.rowguid = Guid.NewGuid();
+                    dbContext.Insert<Address>(_item);
+                    Address _fined = dbContext.Find<Address>(22);
+                    dbContext.Delete<Address>(_fined);
+                    dbContext.Commit();
+                }
+                
+                catch(Exception)
+                {
+                    dbContext.Rollback();
+                }
+                
+                finally
+                {
+                    Address _xiangtan = dbContext.FindAll<Address>(ent => ent.City == "Xiangtan").FirstOrDefault();
+                    Assert.IsNull(_xiangtan);
+                }
+            }
+            
+            using(var dbContext = new AdventureWorks2014DbContext())
+            {
+                dbContext.BeginTransaction();
+                
+                try
+                {
+                    dbContext.BeginTransaction();
+                    Address _jiaxing = new Address();
+                    _jiaxing.PostalCode = "78019";
+                    _jiaxing.AddressLine1 = "Tianxin";
+                    _jiaxing.City = "Jiaxing";
+                    _jiaxing.StateProvinceID = 78;
+                    _jiaxing.PostalCode = "72228";
+                    _jiaxing.rowguid = Guid.NewGuid();
+                    dbContext.Insert<Address>(_jiaxing);
+                    Address _hangzhou = new Address();
+                    _hangzhou.PostalCode = "78019";
+                    _hangzhou.AddressLine1 = "Tianxin";
+                    _hangzhou.City = "HangZhou";
+                    _hangzhou.StateProvinceID = 78;
+                    _hangzhou.PostalCode = "72228";
+                    _hangzhou.rowguid = Guid.NewGuid();
+                    dbContext.Insert<Address>(_hangzhou);
+                    dbContext.Commit();
+                }
+                
+                catch(Exception)
+                {
+                    dbContext.Rollback();
+                }
+                
+                finally
+                {
+                    Address _hangZhou = dbContext.FindAll<Address>(ent => ent.City == "HangZhou").FirstOrDefault();
+                    Assert.IsNotNull(_hangZhou);
+                    Address _jiaxing = dbContext.FindAll<Address>(ent => ent.City == "Jiaxing").FirstOrDefault();
+                    Assert.IsNotNull(_jiaxing);
+                }
             }
         }
     }
