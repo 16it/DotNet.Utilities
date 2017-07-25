6935c1b1a614cf3573598b97486159215fb99826
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-07-25 16:07:59 +0800

    coding

---
+++ YanZhiwei.DotNet.Core.Infrastructure.Mvc/MvcContainerManager.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System.Web;
+using YanZhiwei.DotNet.Core.Infrastructure.DependencyManagement;
+
+namespace YanZhiwei.DotNet.Core.Infrastructure.Mvc
+{
+    /// <summary>
+    /// Mvc ContainerManager
+    /// </summary>
+    /// <seealso cref="YanZhiwei.DotNet.Core.Infrastructure.DependencyManagement.ContainerManager" />
+    public sealed class MvcContainerManager : ContainerManager
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="container">IContainer</param>
+        public MvcContainerManager(IContainer container) : base(container)
+        {
+        }
+
+        /// <summary>
+        /// 获取生命周期
+        /// </summary>
+        /// <returns>
+        /// Scope
+        /// </returns>
+        public override ILifetimeScope Scope()
+        {
+            if (HttpContext.Current != null) //mvc情况
+                return AutofacDependencyResolver.Current.RequestLifetimeScope;
+            return base.Scope();
+        }
+    }
+}
