76b2096ca8d1999081be520be298a939526347e0
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-08-07 15:39:24 +0800

    coding

--- YanZhiwei.DotNet4.Utilities/Core/Retry.cs
+++ YanZhiwei.DotNet4.Utilities/Core/Retry.cs
@@ -3,14 +3,14 @@
     using System;
     using System.Collections.Generic;
     using System.Threading;
-    
+
     /// <summary>
     /// 重试机制
     /// </summary>
     public class Retry
     {
         #region Methods
-        
+
         /// <summary>
         /// 重试机制
         /// </summary>
@@ -20,55 +20,45 @@
         /// <param name="retryCount">重试次数</param>
         /// <param name="expectedResult">期待的结果</param>
         /// <param name="isExpectedResultEqual">是否期待结果一致</param>
-        /// <param name="isSuppressException">
-        /// Suppress exception is true / false</param>
+        /// <param name="isSuppressException">是否不处理异常</param>
         /// <returns>返回值</returns>
-        public static TResult Execute<TResult>(
-            Func<TResult> action,
-            TimeSpan retryInterval,
-            int retryCount,
-            TResult expectedResult,
-            bool isExpectedResultEqual = true,
-            bool isSuppressException = true
-        )
+        public static TResult Execute<TResult>(Func<TResult> action, TimeSpan retryInterval, int retryCount, TResult expectedResult, bool isExpectedResultEqual = true, bool isSuppressException = true)
         {
-            TResult result = default(TResult);
-            bool succeeded = false;
-            var exceptions = new List<Exception>();
-            
-            for(int retry = 0; retry < retryCount; retry++)
+            TResult _result = default(TResult);
+            bool _succeeded = false;
+            List<Exception> _exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
             {
                 try
                 {
-                    if(retry > 0)
-                        Thread.Sleep(retryInterval);
-                        
-                    // Execute method
-                    result = action();
-                    
-                    if(isExpectedResultEqual)
-                        succeeded = result.Equals(expectedResult);
-                        
+                    _result = action();
+
+                    if (isExpectedResultEqual)
+                        _succeeded = _result.Equals(expectedResult);
                     else
-                        succeeded = !result.Equals(expectedResult);
+                        _succeeded = !_result.Equals(expectedResult);
+
+                    if (!_succeeded && (retry + 1) < retryCount)
+                    {
+                        Thread.Sleep(retryInterval);
+                    }
                 }
-                
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    exceptions.Add(ex);
+                    _exceptions.Add(ex);
                 }
-                
-                if(succeeded)
-                    return result;
+
+                if (_succeeded)
+                    return _result;
             }
-            
-            if(!isSuppressException)
-                throw new AggregateException(exceptions);
-                
+
+            if (!isSuppressException && _exceptions.Count > 0)
+                throw new AggregateException(_exceptions);
             else
-                return result;
+                return _result;
         }
-        
+
         #endregion Methods
     }
 }
