21df192cb4f137ff0f1dd8800277b847c9d07c13
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-08-07 18:19:54 +0800

    coding

---
+++ YanZhiwei.DotNet4.Utilities/Attribute/ChineseAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YanZhiwei.DotNet2.Utilities.Model;
+
+namespace YanZhiwei.DotNet4.Utilities.Attribute
+{
+    ///<summary>
+    /// 中文约束Attribute
+    /// </summary>
+    public class ChineseAttribute : RegularExpressionAttribute
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public ChineseAttribute()
+            : base(RegexPattern.ChineseCheck)
+        {
+            ErrorMessage = "请输入中文";
+        }
+    }
+}
