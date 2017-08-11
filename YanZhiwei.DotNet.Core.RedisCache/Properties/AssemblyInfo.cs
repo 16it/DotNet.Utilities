f636dfb3786c54eb561573fd15e46151391257fd
YanZhiwei <Yan.Zhiwei@hotmail.com>
2017-08-11 17:28:45 +0800

    coding

--- YanZhiwei.DotNet.Core.RedisCache/Properties/AssemblyInfo.cs
+++ YanZhiwei.DotNet.Core.RedisCache/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("YanZhiwei.DotNet.Core.RedisCache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Redis缓存")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("YanZhiwei")]
 [assembly: AssemblyProduct("YanZhiwei.DotNet.Core.RedisCache")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © YanZhiwei 2017")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("zh-cn")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
 //对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
@@ -31,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
