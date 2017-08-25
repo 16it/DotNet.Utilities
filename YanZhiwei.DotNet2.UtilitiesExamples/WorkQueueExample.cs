cb51c3c1121ca238a03118c4737a81dfbf7702f6
言志伟 <Yan.Zhiwei@hotmail.com>
2017-08-25 16:45:39 +0800

    coding

--- YanZhiwei.DotNet2.UtilitiesExamples/WorkQueueExample.cs
+++ YanZhiwei.DotNet2.UtilitiesExamples/WorkQueueExample.cs
@@ -7,34 +7,34 @@ namespace YanZhiwei.DotNet2.UtilitiesExamples
 {
     public class WorkQueueExample
     {
-        private static void Main(string[] args)
-        {
-            try
-            {
-                WorkQueue<int> workQueue = new WorkQueue<int>(1000, true);
-                workQueue.OnUserWorkHandlerEvent += WorkQueue_OnUserWorkHandlerEvent;
-                ThreadPool.QueueUserWorkItem(o =>
-                {
-                    for(int i = 0; i < 1000; i++)
-                    {
-                        workQueue.EnqueueItem(i);
-                    }
-                });
-                Console.ReadLine();
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                Console.ReadLine();
-            }
-        }
-        
-        private static void WorkQueue_OnUserWorkHandlerEvent(object sender, EnqueueEventArgs<int> e)
-        {
-            Console.WriteLine("ThreadId:" + Thread.CurrentThread.ManagedThreadId + "，Item:" + e.Item);
-        }
+        //private static void Main(string[] args)
+        //{
+        //    try
+        //    {
+        //        WorkQueue<int> workQueue = new WorkQueue<int>(1000, true);
+        //        workQueue.OnUserWorkHandlerEvent += WorkQueue_OnUserWorkHandlerEvent;
+        //        ThreadPool.QueueUserWorkItem(o =>
+        //        {
+        //            for(int i = 0; i < 1000; i++)
+        //            {
+        //                workQueue.EnqueueItem(i);
+        //            }
+        //        });
+        //        Console.ReadLine();
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        Console.ReadLine();
+        //    }
+        // }
+
+        //private static void WorkQueue_OnUserWorkHandlerEvent(object sender, EnqueueEventArgs<int> e)
+        //{
+        //    Console.WriteLine("ThreadId:" + Thread.CurrentThread.ManagedThreadId + "，Item:" + e.Item);
+        //}
     }
 }
