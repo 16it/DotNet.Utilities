ade962eaff23f8783c0adb9dee897c505ab53a0e
言志伟 <Yan.Zhiwei@hotmail.com>
2017-08-25 17:01:47 +0800

    coding

---
+++ YanZhiwei.DotNet4.Utilities/Model/UdpReceived.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace YanZhiwei.DotNet4.Utilities.Model
+{
+    public struct UdpReceived
+    {
+        public IPEndPoint Sender;
+        public string Message;
+    }
+}
