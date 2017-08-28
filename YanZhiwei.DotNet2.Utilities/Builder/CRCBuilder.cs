7ac8132104483d62fec7bb9fd7de8d7ed87d96db
言志伟 <Yan.Zhiwei@hotmail.com>
2017-08-28 15:45:34 +0800

    coding

--- YanZhiwei.DotNet2.Utilities/Builder/CRCBuilder.cs
+++ YanZhiwei.DotNet2.Utilities/Builder/CRCBuilder.cs
@@ -16,26 +16,48 @@ namespace YanZhiwei.DotNet2.Utilities.Builder
         /// <returns>CRC数值</returns>
         /// 时间：2016/11/23 15:43
         /// 备注：
-        public static ushort Create16MODBUS(byte[] data)
+        public static ushort Calu16MODBUS(byte[] data)
         {
             ushort _ax = 0xFFFF;
             ushort _lsb = 0;
-            
-            for(int i = 0; i < data.Length; i++)
+
+            for (int i = 0; i < data.Length; i++)
             {
                 _ax ^= data[i];
-                
-                for(int j = 0; j < 8; j++)
+
+                for (int j = 0; j < 8; j++)
                 {
                     _lsb = Convert.ToUInt16(_ax & 0x0001);
                     _ax = Convert.ToUInt16(_ax >> 1);
-                    
-                    if(_lsb != 0)
+
+                    if (_lsb != 0)
                         _ax ^= 0xA001;
                 }
             }
-            
+
             return _ax;
+        }
+
+        /// <summary>
+        /// CRC 累加和计算
+        /// </summary>
+        /// <param name="data">需要计算得数组</param>
+        /// <returns>CRC数值</returns>
+        public static byte CaluSum(Byte[] data)
+        {
+            byte _cal = 0x00;
+            uint _totol = 0;
+            foreach (Byte item in data)
+            {
+                _totol += item;
+            }
+
+            unchecked
+            {
+                _cal = (byte)_totol;
+            }
+
+            return _cal;
         }
     }
 }
