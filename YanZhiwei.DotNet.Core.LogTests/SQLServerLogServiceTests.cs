09ccaadf3d495899d871cbb8e1cbc8c89e5afd7d
言志伟 <Yan.Zhiwei@hotmail.com>
2017-09-01 14:18:47 +0800

    coding

--- YanZhiwei.DotNet.Core.LogTests/SQLServerLogServiceTests.cs
+++ YanZhiwei.DotNet.Core.LogTests/SQLServerLogServiceTests.cs
@@ -27,29 +27,10 @@ namespace YanZhiwei.DotNet.Core.Log.Tests
             _logData.Age = 1;
             _logData.Name = "hell world.";
             LogHelper.Debug<Person>(_logData);
-
         }
 
         [TestMethod()]
         public void ErrorTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void ErrorTest1()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void ErrorTest2()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void ErrorTest3()
         {
             Assert.Fail();
         }
@@ -61,67 +42,13 @@ namespace YanZhiwei.DotNet.Core.Log.Tests
         }
 
         [TestMethod()]
-        public void FatalTest1()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void FatalTest2()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void FatalTest3()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
         public void InfoTest()
         {
             Assert.Fail();
         }
 
         [TestMethod()]
-        public void InfoTest1()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void InfoTest2()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void InfoTest3()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
         public void WarnTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void WarnTest1()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void WarnTest2()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void WarnTest3()
         {
             Assert.Fail();
         }
