0ef4d06245478cd0ff50c3acf419e21dffd2472e
言志伟 <Yan.Zhiwei@hotmail.com>
2017-09-04 17:03:58 +0800

    coding

--- YanZhiwei.DotNet.Dapper.UtilitiesTests/DapperSQLiteOperatorTests.cs
+++ YanZhiwei.DotNet.Dapper.UtilitiesTests/DapperSQLiteOperatorTests.cs
@@ -31,25 +31,27 @@ namespace YanZhiwei.DotNet.Dapper.Utilities.Tests
 
             SQLiteHelper.ExecuteNonQuery(_sql);
 
-            _sql = @"CREATE TABLE IF NOT EXISTS [JdfEvents] (
-            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            [OrderCmd] smallint NOT NULL,
-            [OrderSeqNo] smallint NOT NULL,
-            [TimeStamps] TIMESTAMP NOT NULL,
-            [ProtocolVer] varchar(4) NOT NULL,
-            [SourceAddr] varchar(12) NOT NULL,
-            [DescAddr] varchar(12) NOT NULL,
-            [SystemAddr] smallint NOT NULL,
-            [FullPackageDataHexString] varchar(2000) NOT NULL,
-            [ComponentAddr] smallint NOT NULL,
-            [ComponentType] smallint NOT NULL,
-            [CtuCh] smallint NOT NULL,
-            [NetworkNodeAddr] smallint NOT NULL,
-            [PropertyName] NVARCHAR(128) NOT NULL,
-            [PropertyValue] INTEGER NOT NULL,
-            [HasPropertyValue] bit NOT NULL,
-            [CreateTime] TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
-            SQLiteHelper.ExecuteNonQuery(_sql);
+            string _sqlJDFEvents = @"CREATE TABLE [JDFEvents] 
+( [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+  [OrderCmd] smallint NOT NULL, 
+  [OrderSeqNo] smallint NOT NULL, 
+  [TimeStamps] TIMESTAMP NOT NULL, 
+  [ProtocolVer] varchar(4) NOT NULL, 
+  [SourceAddr] varchar(12) NOT NULL, 
+  [DescAddr] varchar(12) NOT NULL, 
+  [SystemAddr] nvarchar(60) NOT NULL, 
+  [FullPackageDataHexString] varchar(500) NOT NULL, 
+  [ComponentAddr] nvarchar(60) NOT NULL, 
+  [SystemFlag] nvarchar(60) NOT NULL, 
+  [ComponentType] smallint NOT NULL, 
+  [CtuCh] smallint NOT NULL, 
+  [NetworkNodeAddr] smallint NOT NULL, 
+  [PropertyName] NVARCHAR(128) , 
+  [PropertyValue] nvarchar(60) , 
+  [HasPropertyValue] bit NOT NULL, 
+  [CreateTime] TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+            SQLiteHelper.ExecuteNonQuery(_sqlJDFEvents);
         }
 
         [TestMethod()]
@@ -68,41 +70,41 @@ namespace YanZhiwei.DotNet.Dapper.Utilities.Tests
             //});
             //Assert.IsTrue(_acutal == 1);
 
-            string _sql = @"INSERT INTO JdfEvents(OrderCmd, 
-                                                  OrderSeqNo, 
-                                                  TimeStamps,
-                                                  ProtocolVer,
-                                                  SourceAddr,
-                                                  DescAddr,
-                                                  SystemAddr,
-                                                  FullPackageDataHexString,
-                                                  ComponentAddr,
-                                                  ComponentType,
-                                                  CtuCh,
-                                                  NetworkNodeAddr,
-                                                  PropertyName,
-                                                  PropertyValue,
-                                                  HasPropertyValue)
-            VALUES(@OrderCmd, @OrderSeqNo, @TimeStamps,@ProtocolVer,@SourceAddr,@DescAddr,@SystemAddr,
-                   @FullPackageDataHexString,@ComponentAddr,@ComponentType,@CtuCh,@NetworkNodeAddr,@PropertyName,@PropertyValue,@HasPropertyValue)";
+            //string _sql = @"INSERT INTO JdfEvents(OrderCmd, 
+            //                                      OrderSeqNo, 
+            //                                      TimeStamps,
+            //                                      ProtocolVer,
+            //                                      SourceAddr,
+            //                                      DescAddr,
+            //                                      SystemAddr,
+            //                                      FullPackageDataHexString,
+            //                                      ComponentAddr,
+            //                                      ComponentType,
+            //                                      CtuCh,
+            //                                      NetworkNodeAddr,
+            //                                      PropertyName,
+            //                                      PropertyValue,
+            //                                      HasPropertyValue)
+            //VALUES(@OrderCmd, @OrderSeqNo, @TimeStamps,@ProtocolVer,@SourceAddr,@DescAddr,@SystemAddr,
+            //       @FullPackageDataHexString,@ComponentAddr,@ComponentType,@CtuCh,@NetworkNodeAddr,@PropertyName,@PropertyValue,@HasPropertyValue)";
 
-            EventModel _model = new EventModel();
-            _model.OrderCmd = 0x99;
-            _model.OrderSeqNo = 99;
-            _model.TimeStamps = DateTime.Now.AddHours(-1);
-            _model.ProtocolVer = "0101";
-            _model.SourceAddr = "170102030405";
-            _model.DescAddr = "170102030400";
-            _model.SystemAddr = 0x01;
-            _model.FullPackageDataHexString = "01 02 03 04 05 06 07 08 09";
-            _model.ComponentAddr = 02;
-            _model.ComponentType = 03;
-            _model.CtuCh = 04;
-            _model.NetworkNodeAddr = 05;
-            _model.PropertyName = "启动";
-            _model.PropertyValue = 0x01;
-            _model.HasPropertyValue = true;
-            SQLiteHelper.ExecuteNonQuery<EventModel>(_sql, _model);
+            //EventModel _model = new EventModel();
+            //_model.OrderCmd = 0x99;
+            //_model.OrderSeqNo = 99;
+            //_model.TimeStamps = DateTime.Now.AddHours(-1);
+            //_model.ProtocolVer = "0101";
+            //_model.SourceAddr = "170102030405";
+            //_model.DescAddr = "170102030400";
+            //_model.SystemAddr = 0x01;
+            //_model.FullPackageDataHexString = "01 02 03 04 05 06 07 08 09";
+            //_model.ComponentAddr = 02;
+            //_model.ComponentType = 03;
+            //_model.CtuCh = 04;
+            //_model.NetworkNodeAddr = 05;
+            //_model.PropertyName = "启动";
+            //_model.PropertyValue = 0x01;
+            //_model.HasPropertyValue = true;
+            //SQLiteHelper.ExecuteNonQuery<EventModel>(_sql, _model);
         }
     }
 }
