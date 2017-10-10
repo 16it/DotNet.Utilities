7147d5fd0d4c139bd13555211a46a3cd42324157
言志伟 <Yan.Zhiwei@hotmail.com>
2017-10-10 14:58:47 +0800

    coding

--- YanZhiwei.DotNet.ModbusProtocol.UtilitiesTests/RTUModBusUnPackageTests.cs
+++ YanZhiwei.DotNet.ModbusProtocol.UtilitiesTests/RTUModBusUnPackageTests.cs
@@ -13,14 +13,14 @@ namespace YanZhiwei.DotNet.ModbusProtocol.Utilities.Tests
             RTUModBusUnPackage _unpackageOrderCmd0x01 = new RTUModBusUnPackage();
             SlaveReplyDataBase _slaveReplyData = null;
             UnPackageError _unpackageError = _unpackageOrderCmd0x01.BuilderObjFromBytes(_orderCmd0x01, out _slaveReplyData);
-            Assert.AreEqual(_unpackageOrderCmd0x01.OrderCmd, 0x01);
+            Assert.AreEqual(_slaveReplyData.OrderCmdCode, 0x01);
 
 
             byte[] _orderCmd0x03 = { 002, 0x03, 0x14, 0x00, 0x01, 0x00, 0x02, 0x00, 0x03, 0x00, 0x04, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x37, 0x57 };
             RTUModBusUnPackage _unpackageOrderCmd0x03 = new RTUModBusUnPackage();
             _slaveReplyData = null;
             _unpackageError = _unpackageOrderCmd0x03.BuilderObjFromBytes(_orderCmd0x03, out _slaveReplyData);
-            Assert.AreEqual(_unpackageOrderCmd0x03.OrderCmd, 0x03);
+            Assert.AreEqual(_slaveReplyData.OrderCmdCode, 0x03);
         }
     }
 }
