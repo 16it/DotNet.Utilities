69645fc46989d0c2730c81c64458a6b9b3b4db35
言志伟 <Yan.Zhiwei@hotmail.com>
2017-10-16 13:30:06 +0800

    coding

---
+++ YanZhiwei.DotNet3.5.Utilities/Common/ArrayHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace YanZhiwei.DotNet3._5.Utilities.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Array 辅助类
+    /// </summary>
+    public static class ArrayHelper
+    {
+        #region Methods
+
+        /// <summary>
+        /// 将array转为具体List对象集合
+        /// </summary>
+        /// <param name="data">Array</param>
+        /// <returns>List对象集合</returns>
+        public static List<T> ToList<T>(this Array data)
+            where T : class, new()
+        {
+            return data.Cast<T>().ToList<T>();
+        }
+
+        #endregion Methods
+    }
+}
