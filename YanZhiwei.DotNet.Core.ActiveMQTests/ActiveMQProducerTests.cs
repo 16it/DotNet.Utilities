52afc3af340e29dd54237782019902355ffbc6bd
言志伟 <Yan.Zhiwei@hotmail.com>
2017-10-31 22:32:13 +0800

    coding

---
+++ YanZhiwei.DotNet.Core.ActiveMQTests/ActiveMQProducerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YanZhiwei.DotNet.Core.ActiveMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YanZhiwei.DotNet.Core.ActiveMQ.Tests
+{
+    [TestClass()]
+    public class ActiveMQProducerTests
+    {
+        [TestMethod()]
+        public void CloseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OpenTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendMessageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendMessageTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
