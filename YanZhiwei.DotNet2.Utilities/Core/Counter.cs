bdd5e38769151f18e488d3b4b773906e56af1f66
言志伟 <Yan.Zhiwei@hotmail.com>
2018-01-16 19:16:43 +0800

    coding

---
+++ YanZhiwei.DotNet2.Utilities/Core/Counter.cs
@@ -0,0 +1,46 @@
+﻿namespace YanZhiwei.DotNet2.Utilities.Core
+{
+    using System.Threading;
+
+    /// <summary>
+    /// 计数器
+    /// </summary>
+    public sealed class Counter
+    {
+        #region Fields
+
+        private int current = 0;
+
+        #endregion Fields
+
+        #region Methods
+
+        /// <summary>
+        /// 加一
+        /// </summary>
+        /// <returns>当前数值</returns>
+        public int NextValue()
+        {
+            return Interlocked.Increment(ref this.current);
+        }
+
+        /// <summary>
+        /// 重置为0
+        /// </summary>
+        public void Reset()
+        {
+            Reset(0);
+        }
+
+        /// <summary>
+        /// 重置
+        /// </summary>
+        /// <param name="resetValue">重置数值</param>
+        public void Reset(int resetValue)
+        {
+            Interlocked.Exchange(ref current, resetValue);
+        }
+
+        #endregion Methods
+    }
+}
