5d0d42c4b578884e4540ac6ac445e06f6efb0414
言志伟 <Yan.Zhiwei@hotmail.com>
2018-05-10 14:06:11 +0800

    coding

---
+++ YanZhiwei.DotNet3.5.Utilities/WPF/NotificationHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace YanZhiwei.DotNet3._5.Utilities.WPF
+{
+    using System;
+    using System.Linq.Expressions;
+
+    internal static class NotificationHelper
+    {
+        #region Methods
+
+        public static void NotifyPropertyChanged<T, TProperty>(this T model, Expression<Func<T, TProperty>> keySelector)
+            where T : NotificationObject
+        {
+            var _propertyName = string.Empty;
+            if ((keySelector.Body as UnaryExpression) != null)
+            {
+                _propertyName = ((keySelector.Body as UnaryExpression).Operand as MemberExpression).Member.Name;
+            }
+            else if ((keySelector.Body as MemberExpression) != null)
+            {
+                _propertyName = ((keySelector.Body as MemberExpression).Member.Name);
+            }
+            if (!string.IsNullOrEmpty(_propertyName))
+            {
+                model.NotifyChanges(_propertyName);
+            }
+        }
+
+        #endregion Methods
+    }
+}
--- YanZhiwei.DotNet3.5.Utilities/WPF/NotificationObject.cs
+++ YanZhiwei.DotNet3.5.Utilities/WPF/NotificationObject.cs
@@ -1,33 +1,23 @@
-﻿using System.ComponentModel;
-
-namespace YanZhiwei.DotNet3._5.Utilities.WPF
+﻿namespace YanZhiwei.DotNet3._5.Utilities.WPF
 {
-    /// <summary>
-    /// 绑定数据属性。这个类的作用是实现了INotifyPropertyChanged接口。WPF中类要实现这个接口，其属性成员才具备通知UI的能力，数据绑定。
-    /// </summary>
-    /// 时间：2016/7/4 16:41
-    /// 备注：
-    public class NotificationObject : INotifyPropertyChanged
+    using System.ComponentModel;
+
+    internal class NotificationObject : INotifyPropertyChanged
     {
-        /// <summary>
-        /// 属性数值更新事件
-        /// </summary>
-        /// 时间：2016/7/4 16:41
-        /// 备注：
+        #region Events
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        /// <summary>
-        /// 触发属性数值更新事件
-        /// </summary>
-        /// <param name="propertyName">Name of the property.</param>
-        /// 时间：2016/7/4 16:41
-        /// 备注：
-        public void RaisePropertyChanged(string propertyName)
+        #endregion Events
+
+        #region Methods
+
+        public void NotifyChanges(string propertyName)
         {
-            if (this.PropertyChanged != null)
-            {
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion Methods
     }
 }
